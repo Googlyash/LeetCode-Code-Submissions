d67082fe2c7ac01c58ac902b864fb1c42566a178
Googlyash <77059584+Googlyash@users.noreply.github.com>
2023-06-09 18:54:13 +0530

    Time: 15 ms (75.46%) | Memory: 15.9 MB (32.10%) - LeetSync

--- find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.cpp
+++ find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.cpp
@@ -1,10 +1,14 @@
 class Solution {
 public:
     char nextGreatestLetter(vector<char>& letters, char target) {
-        for(int i=0;i<letters.size();i++)
+        int n=letters.size();
+        int l=0,r=n-1,mid;
+        while(l<=r)
         {
-            if(letters[i]>target)return letters[i];
+            mid=l+(r-l)/2;
+            if(letters[mid]<=target)l=mid+1;
+            else r=mid-1;
         }
-        return letters[0];
+        return (l>=letters.size() || r<0)? letters[0]:letters[l];
     }
 };
