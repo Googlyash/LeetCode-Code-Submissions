41adb68c12657e1f10ffbfcb3aeae9c61ccb63ad
Googlyash <77059584+Googlyash@users.noreply.github.com>
2023-06-09 18:57:23 +0530

    Time: 15 ms (75.46%) | Memory: 15.9 MB (72.43%) - LeetSync

--- find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.cpp
+++ find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.cpp
@@ -9,6 +9,6 @@ public:
             if(letters[mid]<=target)l=mid+1;
             else r=mid-1;
         }
-        return (l>=letters.size() || r<0)? letters[0]:letters[l];
+        return (l>=letters.size())? letters[0]:letters[l];
     }
 };
