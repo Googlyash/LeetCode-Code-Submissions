d9298d39f72b98bf63bb9168dc915a9399d740ae
Googlyash <77059584+Googlyash@users.noreply.github.com>
2023-06-15 05:50:29 +0530

    Time: 16 ms (68.81%) | Memory: 13.2 MB (84.89%) - LeetSync

--- set-matrix-zeroes/set-matrix-zeroes.cpp
+++ set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,20 +1,44 @@
 class Solution {
 public:
-    void setZeroes(vector<vector<int>>& v) {
-        int n=v.size();
-        int m=v[0].size();
-        vector<pair<int,int>>a;
+    void setZeroes(vector<vector<int>>& a) {
+        int n=a.size();
+        int m=a[0].size();
+
+        bool firstRow=0,firstCol=0;
         for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(v[i][j]==0){
-                    a.push_back({i,j});
+            if(a[i][0]==0){
+                firstCol=true;
+                break;
+            }
+        }
+
+        for(int i=0;i<m;i++){
+            if(a[0][i]==0){
+                firstRow=true;
+                break;
+            }
+        }
+
+        for(int i=1;i<n;i++){
+            for(int j=1;j<m;j++){
+                if(!a[i][j]){
+                    a[i][0]=0;
+                    a[0][j]=0;
                 }
             }
         }
-        for(int i=0;i<a.size();i++)
-        {
-            for(int j=0;j<m;j++)v[a[i].first][j]=0;
-            for(int j=0;j<n;j++)v[j][a[i].second]=0;
+        for(int i=1;i<n;i++){
+            for(int j=1;j<m;j++){
+                if(!a[i][0] || !a[0][j])a[i][j]=0;
+            }
+        }
+
+        if(firstCol){
+            for(int i=0;i<n;i++)a[i][0]=0;
+        }
+
+        if(firstRow){
+            for(int i=0;i<m;i++)a[0][i]=0;
         }
     }
 };
