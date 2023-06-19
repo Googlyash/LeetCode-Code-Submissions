f40164b013179c521ea730ea0c4705922801f552
Googlyash <77059584+Googlyash@users.noreply.github.com>
2023-06-19 17:49:21 +0530

    Time: 6 ms (8.02%) | Memory: 6.8 MB (39.02%) - LeetSync

--- pascals-triangle/pascals-triangle.cpp
+++ pascals-triangle/pascals-triangle.cpp
@@ -1,13 +1,16 @@
+//Striver
+//Self-Solved
+
 class Solution {
 public:
     vector<vector<int>> generate(int n) {
         vector<vector<int>> v;
-        int i=0,k,j=0;
         v.push_back({1});
-        for(i=1;i<n;i++){
+
+        for(int i=1;i<n;i++){
             vector<int>vv;
             vv.push_back(1);
-            for(j=1;j<i;j++){
+            for(int j=1;j<i;j++){
                 vv.push_back(v[i-1][j]+v[i-1][j-1]);
             }
             vv.push_back(1);
