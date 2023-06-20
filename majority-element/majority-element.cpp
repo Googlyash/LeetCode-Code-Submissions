3ef51a50d85916e85587432c601af9178d09bc78
Googlyash <77059584+Googlyash@users.noreply.github.com>
2023-06-20 16:06:48 +0530

    Time: 23 ms (53.57%) | Memory: 19.4 MB (95.84%) - LeetSync

--- majority-element/majority-element.cpp
+++ majority-element/majority-element.cpp
@@ -1,23 +1,13 @@
 //Striver day3
+//Comments
 //Self
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
         sort(nums.begin(),nums.end());
-        int idx=0,mx=0,allmx=0;
-        for(int i=1;i<nums.size();i++){
-            if(nums[i]!=nums[i-1]){
-                if(mx>allmx){
-                    allmx=mx;
-                    idx=i-1;
-                }
-                mx=0;
-            }
-            mx++;
-        }
-        if(mx>allmx){
-            allmx=mx;
-            idx=nums.size()-1;
+        int idx=nums.size()/2,mx=0,allmx=0;
+        for(int i=nums.size()/2;i<nums.size();i++){
+            if(nums[i]==nums[nums.size()/2])idx=i;
         }
         return nums[idx];
     }
