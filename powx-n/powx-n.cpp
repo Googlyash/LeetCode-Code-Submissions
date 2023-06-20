fddf00b6e06fa3162a514f9029c61ab4bf3d58c3
Googlyash <77059584+Googlyash@users.noreply.github.com>
2023-06-20 14:24:05 +0530

    Time: 4 ms (42.32%) | Memory: 6.1 MB (19.80%) - LeetSync

---
+++ powx-n/powx-n.cpp
@@ -0,0 +1,11 @@
+//Striver day3
+//Self-solved
+
+class Solution {
+public:
+    double myPow(double x, int n) {
+        double ans=1.00, mod=1e9;
+        ans=pow(x,n);
+        return ans;
+    }
+};
