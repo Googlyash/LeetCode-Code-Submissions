787621971679d9195422fa609a4f66efb8587c9f
Googlyash <77059584+Googlyash@users.noreply.github.com>
2023-06-20 16:02:34 +0530

    Time: 32 ms (7.19%) | Memory: 19.6 MB (58.20%) - LeetSync

---
+++ majority-element/majority-element.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int idx=0,mx=0,allmx=0;
+        for(int i=1;i<nums.size();i++){
+            if(nums[i]!=nums[i-1]){
+                if(mx>allmx){
+                    allmx=mx;
+                    idx=i-1;
+                }
+                mx=0;
+            }
+            mx++;
+        }
+        if(mx>allmx){
+            allmx=mx;
+            idx=nums.size()-1;
+        }
+        return nums[idx];
+    }
+};
