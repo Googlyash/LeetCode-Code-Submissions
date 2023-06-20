7f26e3969a82686062404b0b2969e96ffb9731cb
Googlyash <77059584+Googlyash@users.noreply.github.com>
2023-06-20 10:07:12 +0530

    Time: 116 ms (79.25%) | Memory: 61.2 MB (46.18%) - LeetSync

--- find-the-duplicate-number/find-the-duplicate-number.cpp
+++ find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -1,28 +1,24 @@
 //Striver day2
 //Comments
+//Not Self Solved
 
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        int n=nums.size();
-        int lo=1;
-        int hi=n-1;
-        while(lo<hi){
-            int mid = lo+(hi-lo)/2;
-            int cnt=0;
-
-            for(int i=0;i<n;i++){
-                if(nums[i]<=mid){
-                    cnt++;
-                }
+        int duplicate = -1;
+        for (int i = 0; i < nums.size(); i++) {
+            int cur = abs(nums[i]);
+            if (nums[cur] < 0) {
+                duplicate = cur;
+                break;
             }
-            if(cnt<=mid){
-                lo=mid+1;
-            }
-            else {
-                hi=mid;
-            }
+            nums[cur] *= -1;
         }
-        return lo;
+        
+        // Restore numbers
+        for (auto& num : nums)
+            num = abs(num);
+        
+        return duplicate;
     }
 };
