7d469f47d41d0ac4e96ff522fb60e5df0ddaa167
Googlyash <77059584+Googlyash@users.noreply.github.com>
2023-06-20 14:10:11 +0530

    Time: 3 ms (85.00%) | Memory: 9.5 MB (24.80%) - LeetSync

--- search-a-2d-matrix/search-a-2d-matrix.cpp
+++ search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -1,5 +1,6 @@
 //Striver day3
-//Self solved
+//Comments
+//Self-solved earlier
 //Lode lag gaye
 class Solution {
 public:
@@ -7,19 +8,18 @@ public:
         int n=matrix.size(), m=matrix[0].size(),i;
         int l=0,r=n-1;
         while(l<r){
-            int mid=l+(r-l)/2;
-
-            if(matrix[mid][0]== target)return 1;
-            else if(matrix[mid][0]>target)r=mid;
-            else l=mid+1;
+            int mid=(r+l+1)/2;
+            if(matrix[mid][0] >target)r=mid-1;
+            else l=mid;
         }
         if(matrix[l][0]== target)return 1;
+
         int lc=0,rc=m-1;
-        if(matrix[l][0] >target)l=max(0,l-1);
+
         while(lc<rc){
             int mid=lc+(rc-lc)/2;
             if(matrix[l][mid]==target)return 1;
-            else if(matrix[l][mid]> target)rc=mid;
+            else if(matrix[l][mid]>= target)rc=mid;
             else lc=mid+1;
         }
         if(matrix[l][lc]==target)return 1;
