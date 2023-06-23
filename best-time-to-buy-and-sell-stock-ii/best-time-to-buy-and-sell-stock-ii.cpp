358bc8bdd73569970107fcd084c89c9720fa424b
Googlyash <77059584+Googlyash@users.noreply.github.com>
2023-06-23 11:45:49 +0530

    Time: 4 ms (85.08%) | Memory: 12.9 MB (79.08%) - LeetSync

---
+++ best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n=prices.size();
+        int pro=0,hisell=prices[n-1],currpro=0;
+        for(int i=n-1;i>=0;i--){
+            if(prices[i]> hisell){
+                hisell=prices[i];
+                pro+=currpro;
+                currpro=0;
+            }
+            else{
+                currpro+=hisell-prices[i];
+                hisell=prices[i];
+            }
+        }
+        return pro+currpro;
+    }
+};
