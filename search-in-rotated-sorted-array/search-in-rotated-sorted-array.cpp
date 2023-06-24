d5f7abeca7549d2ccdb5f592b70beff3aa9be448
Googlyash <77059584+Googlyash@users.noreply.github.com>
2023-06-25 00:07:32 +0530

    Time: 7 ms (31.27%) | Memory: 11 MB (95.65%) - LeetSync

---
+++ search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -0,0 +1,27 @@
+//Striver Binary Search
+
+class Solution {
+public:
+    int search(vector<int>& arr, int k) {
+        int n=arr.size();
+        int l=0,r=n-1;
+        while(l<=r){
+            int mid=l+(r-l)/2;
+
+            if(arr[mid]==k)return mid;
+
+            if (arr[l]<=arr[mid]){
+                if(arr[l]<=k && k<=arr[mid])
+                r=mid-1;
+                else l=mid+1;
+            }
+            else {
+                if(arr[mid]<=k && k<=arr[r])
+                l=mid+1;
+                else r=mid-1;
+            }
+        }
+        
+        return -1;
+    }
+};
