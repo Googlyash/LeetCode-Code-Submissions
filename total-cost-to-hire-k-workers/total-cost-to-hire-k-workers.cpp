8fbf7d95df56fd96ccce508d67f0d22b485bc3d1
Googlyash <77059584+Googlyash@users.noreply.github.com>
2023-06-26 19:34:51 +0530

    Time: 196 ms (82.48%) | Memory: 72.8 MB (82.21%) - LeetSync

--- total-cost-to-hire-k-workers/total-cost-to-hire-k-workers.cpp
+++ total-cost-to-hire-k-workers/total-cost-to-hire-k-workers.cpp
@@ -1,5 +1,5 @@
-//POTD June,26, 2023
-//Comments
+//POTD June'26, 2023
+//Almost self
 
 class Solution {
 public:
@@ -7,22 +7,26 @@ public:
         priority_queue<int,vector<int>, greater<int> >p,q;
         int n=a.size();
         long long ans=0;
-        int l=0,cnt=0,r=n-1;
-        while(cnt<k){
-            while(p.size()<c && l<=r)p.push(a[l++]);
-            while(q.size()<c && r>=l)q.push(a[r--]);
+        int l=0,r=n-1;
+        while(p.size()<c && l<=r)p.push(a[l++]);
+        while(l<=r && q.size()<c)q.push(a[r--]);
+
+        // return q.size();
+        for(int i=0;i<k;i++){
             int x=p.size()>0?p.top():INT_MAX;
             int y=q.size()>0?q.top():INT_MAX;
 
             if(x<=y){
                 ans+=x;
                 p.pop();
+                while(l<=r && p.size()<c)p.push(a[l++]);
             }
-            else{
+            else
+            {
                 ans+=y;
                 q.pop();
+                while(l<=r && q.size()<c)q.push(a[r--]);
             }
-            cnt++;
         }
         return ans;
     }
