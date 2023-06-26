0655a69d914d49148de693959cce0a6c7fbf667d
Googlyash <77059584+Googlyash@users.noreply.github.com>
2023-06-26 19:02:08 +0530

    Time: 32 ms (44.44%) | Memory: 22.4 MB (35.21%) - LeetSync

---
+++ single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -0,0 +1,20 @@
+//Striver
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        int n=nums.size();
+        int l=0,r=n-1;
+        if(n==1)return nums[0];
+        while(l<=r){
+            int mid=l+(r-l)/2;
+
+            if(nums[mid]==nums[mid^1]){
+                l=mid+1;
+            }
+            else {
+                r=mid-1;
+            }
+        }
+        return nums[l];
+    }
+};
