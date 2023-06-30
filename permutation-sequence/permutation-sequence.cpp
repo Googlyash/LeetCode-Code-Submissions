9394bf4cf7c35c005585aa54dc026e7d935cd6dc
Googlyash <77059584+Googlyash@users.noreply.github.com>
2023-06-30 22:38:36 +0530

    Time: 2 ms (60.50%) | Memory: 6.1 MB (17.09%) - LeetSync

--- permutation-sequence/permutation-sequence.cpp
+++ permutation-sequence/permutation-sequence.cpp
@@ -1,24 +1,25 @@
 //Striver: Recursion
-//June'30, 2023 10:38pm
-
+//June'30 , 2023 10:36 pm
+#include<bits/stdc++.h>
 class Solution {
 public:
     string getPermutation(int n, int k) {
+        vector<int>num;
         string s,s1;
         int fact=1;
-        for(int i=1;i<=n;i++)s+=(i+'0');
+        for(int i=1;i<=n;i++)num.push_back(i);
         for(int i=1;i<n;i++)fact*=i;
         int j=0;
         k--;
         while(1){
             int r=k/fact;
-            s1+=s[r];
-            s.erase(s.begin()+r);
-            if(s.size()==0){
+            s1+=to_string(num[r]);
+            num.erase(num.begin()+k/fact);
+            if(num.size()==0){
                 break;
             }
             k%=fact;
-            fact/=s.size();
+            fact/=num.size();
         }
         // for(int i=0;i<n;i++){
         //     int r= k/fact;
