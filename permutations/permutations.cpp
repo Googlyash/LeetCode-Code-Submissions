334732822361f376fb348497808fb64471bd6fb7
Googlyash <77059584+Googlyash@users.noreply.github.com>
2023-06-30 16:17:36 +0530

    Time: 4 ms (48.00%) | Memory: 7.5 MB (98.37%) - LeetSync

--- permutations/permutations.cpp
+++ permutations/permutations.cpp
@@ -1,30 +1,14 @@
-//Striver:Recursion
-//June'30,2023
-class Solution {
-private:
-    void allPermute(int freq[], vector<int>&ds, vector<vector<int> >&v, vector<int>&nums){
-        if(ds.size()==nums.size()){
-            v.push_back(ds);
-            return;
-        }
+//using STL C++
+//Self-solved
 
-        for(int i=0;i<nums.size();i++){
-            if(freq[i]!=1){
-                ds.push_back(nums[i]);
-                freq[i]=1;
-                allPermute(freq,ds,v,nums);
-                freq[i]=0;
-                ds.pop_back();
-            }
-        }
-    }
+class Solution {
 public:
     vector<vector<int>> permute(vector<int>& nums) {
-        vector<vector<int>> v;
-        vector<int>ds;
-        int freq[nums.size()];
-        for(int i=0;i<nums.size();i++)freq[i]=0;
-        allPermute(freq,ds,v,nums);
+        vector<vector<int>>v;
+        sort(nums.begin(),nums.end());
+        do {
+            v.push_back(nums);
+        } while(next_permutation(nums.begin(),nums.end()));
         return v;
     }
 };
