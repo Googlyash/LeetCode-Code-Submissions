ae4cbea136163aaf74a291b5663e4a3523a914ac
Googlyash <77059584+Googlyash@users.noreply.github.com>
2023-06-30 22:42:15 +0530

    Time: 185 ms (16.63%) | Memory: 5.9 MB (97.51%) - LeetSync

--- permutation-sequence/permutation-sequence.cpp
+++ permutation-sequence/permutation-sequence.cpp
@@ -1,34 +1,17 @@
 //Striver: Recursion
-//June'30 , 2023 10:36 pm
-#include<bits/stdc++.h>
+//Self-solved
+//June'30, 2023 10:41 pm
 class Solution {
 public:
     string getPermutation(int n, int k) {
-        vector<int>num;
-        string s,s1;
-        int fact=1;
-        for(int i=1;i<=n;i++)num.push_back(i);
-        for(int i=1;i<n;i++)fact*=i;
-        int j=0;
-        k--;
-        while(1){
-            int r=k/fact;
-            s1+=to_string(num[r]);
-            num.erase(num.begin()+k/fact);
-            if(num.size()==0){
-                break;
-            }
-            k%=fact;
-            fact/=num.size();
-        }
-        // for(int i=0;i<n;i++){
-        //     int r= k/fact;
-        //     s1+=s[r];
-        //     s.erase(s.begin()+r);
-        //     if(s.size()==0)break;
-        //     k-=r*(int)tgamma(n-i);
-        //     fact/=s.size();
-        // }
-        return s1;
+        string s;
+        for(int i=1;i<=n;i++)s+=i+'0';
+        sort(s.begin(), s.end());                            //sort(s, s+n); doesn't work in strings!!
+        int i=0;
+        do{
+            i++;
+            if(i>=k)break;
+        }while( next_permutation(s.begin(),s.end()));
+        return s;
     }
 };
