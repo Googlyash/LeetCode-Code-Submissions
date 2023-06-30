0391e2f883f7910d13f5668ba44b56f8227f8639
Googlyash <77059584+Googlyash@users.noreply.github.com>
2023-06-30 16:42:34 +0530

    Time: 0 ms (100.00%) | Memory: 7.5 MB (98.37%) - LeetSync

--- permutations/permutations.cpp
+++ permutations/permutations.cpp
@@ -1,30 +1,26 @@
-//Striver:Recursion
-//June'30,2023
+//Striver: Recursion
+//3rd technique
+//Self-solved
+
 class Solution {
 private:
-    void allPermute(int freq[], vector<int>&ds, vector<vector<int> >&v, vector<int>&nums){
-        if(ds.size()==nums.size()){
-            v.push_back(ds);
+    void allPermut(int idx, vector<int>&nums, vector<vector<int>> &v){
+        if(idx==nums.size()){
+            v.push_back(nums);
             return;
         }
 
-        for(int i=0;i<nums.size();i++){
-            if(freq[i]!=1){
-                ds.push_back(nums[i]);
-                freq[i]=1;
-                allPermute(freq,ds,v,nums);
-                freq[i]=0;
-                ds.pop_back();
-            }
+        for(int i=idx;i<nums.size();i++){
+            swap(nums[i],nums[idx]);
+            allPermut(idx+1,nums, v);
+            swap(nums[i],nums[idx]);
         }
     }
 public:
     vector<vector<int>> permute(vector<int>& nums) {
-        vector<vector<int>> v;
-        vector<int>ds;
-        int freq[nums.size()];
-        for(int i=0;i<nums.size();i++)freq[i]=0;
-        allPermute(freq,ds,v,nums);
+        vector<vector<int>>v;
+        sort(nums.begin(),nums.end());
+        allPermut(0,nums,v);
         return v;
     }
 };
