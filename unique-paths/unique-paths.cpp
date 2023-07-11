e953fbd03c9f309fa7d6df91950f519472afd298
Googlyash <77059584+Googlyash@users.noreply.github.com>
2023-07-11 10:57:44 +0530

    Time: 3 ms (38.33%) | Memory: 6 MB (85.59%) - LeetSync

--- unique-paths/unique-paths.cpp
+++ unique-paths/unique-paths.cpp
@@ -5,17 +5,25 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        vector<int>dp(n,0);
-        for(int i=0;i<m;i++){
-            vector<int>curr(n,0);
-            for(int j=0;j<n;j++){
-                if(i==0 || j==0)curr[j]=1;
-                else {
-                    curr[j]=curr[j-1]+dp[j];
-                }
-            }
-            dp=curr;
+        long long ans=1, mod=1e9+7;
+        // n--, m--;
+        for(int i=n;i<m+n-1;i++){
+            ans*=(i);
+            ans/=(i-n+1);
+            // ans%=mod;
         }
+        return ans;
+        // vector<int>dp(n,0);
+        // for(int i=0;i<m;i++){
+        //     vector<int>curr(n,0);
+        //     for(int j=0;j<n;j++){
+        //         if(i==0 || j==0)curr[j]=1;
+        //         else {
+        //             curr[j]=curr[j-1]+dp[j];
+        //         }
+        //     }
+        //     dp=curr;
+        // }
         // for(int i=0;i<m;i++)dp[i][0]=1;
         // for(int i=0;i<n;i++)dp[0][i]=1;
         // for(int i=1;i<m;i++){
@@ -23,6 +31,6 @@ public:
         //         dp[i][j]=dp[i-1][j]+dp[i][j-1];
         //     }
         // }
-        return dp[n-1];
+        // return dp[n-1];
     }
 };
