1433c9fc9865a275fa3b897b6f67a45cb2b7a4c4
Googlyash <77059584+Googlyash@users.noreply.github.com>
2023-08-01 23:25:12 +0530

    Time: 19 ms (87.17%) | Memory: 17.9 MB (35.81%) - LeetSync

---
+++ 77-combinations/combinations.cpp
@@ -0,0 +1,24 @@
+class Solution {
+private:
+    void f(int n, int k, vector<int>&v, vector<vector<int>>& a){
+        if(v.size()==k){
+            a.push_back(v);
+            return;
+        }
+        if(n==0){
+            return;
+        }
+
+        f(n-1,k,v,a);
+        v.push_back(n);
+        f(n-1,k,v,a);
+        v.pop_back();
+    }
+public:
+    vector<vector<int>> combine(int n, int k) {
+        vector<vector<int>>a;
+        vector<int>v;
+        f(n,k,v, a);
+        return a;
+    }
+};
