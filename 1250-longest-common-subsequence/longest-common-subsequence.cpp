feea0c887eaa4636dd9501a64613e07478b82faf
Googlyash <77059584+Googlyash@users.noreply.github.com>
2023-08-05 17:41:28 +0530

    Time: 25 ms (97.58%) | Memory: 6.7 MB (89.15%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,22 +1,25 @@
 //Striver SDE Sheet
 //Aug'5, 2023 05:37 pm
+//Tabulation with space optimization
 
 class Solution {
 public:
     int longestCommonSubsequence(string s1, string s2) {
         int n=s1.size(), m=s2.size(), ans=0;
-        vector<vector<int>>dp(n+1, vector<int>(m+1, 0));
+        // vector<vector<int>>dp(n+1, vector<int>(m+1, 0));
 
+        vector<int>pre(m+1, 0), cur(m+1, 0);
         for(int i=1;i<=n;i ++){
             for(int j=1;j<=m;j++){
                 if(s1[i-1]==s2[j-1]){
-                    dp[i][j]= 1+dp[i-1][j-1];
+                    cur[j]= 1+pre[j-1];
                 }
                 else 
-                dp[i][j]= max(dp[i-1][j], dp[i][j-1]);
+                cur[j]= max(pre[j], cur[j-1]);
             }
+            pre=cur;
         }
 
-        return dp[n][m];
+        return pre[m];
     }
 };
