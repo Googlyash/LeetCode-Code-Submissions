1d26fa2030bff2da4824c58d10da76b1e5bdd4b4
Googlyash <77059584+Googlyash@users.noreply.github.com>
2023-08-05 16:45:58 +0530

    Time: 5 ms (77.89%) | Memory: 7.5 MB (92.68%) - LeetSync

--- 139-word-break/word-break.cpp
+++ 139-word-break/word-break.cpp
@@ -1,16 +1,16 @@
-//Aug'4, 2023 06:27 pm
+//Aug'5, 2023 04:40 pm
 
 class Solution {
 public:
     bool wordBreak(string s, vector<string>& wordDict) {
-        string s1;
-        int n=s.size(), m=wordDict.size(), last=0, temp=0;
-        vector<bool>dp(n+1, false);
+        int n=s.size();
+        vector<bool> dp(n+1, false);
         dp[n]=true;
-        for(int i=n-1;i>-1; i--){
-            for(auto w:wordDict){
-                if(i+ w.size()<=n && s.substr(i, w.size())==w){
-                    dp[i]=dp[i+w.size()];
+
+        for(int i=n-1; i>=0; i--){
+            for(auto word: wordDict){
+                if(i+word.size()<=n && s.substr(i, word.size())==word) {
+                    dp[i]=dp[i+word.size()];
                 }
                 if(dp[i])break;
             }
@@ -18,5 +18,3 @@ public:
         return dp[0];
     }
 };
-
-//NeetCode youtube solution
