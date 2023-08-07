fa0da919fe1d39f5770d6ccd7c0d013c7db05681
Googlyash <77059584+Googlyash@users.noreply.github.com>
2023-08-07 18:50:33 +0530

    Time: 6 ms (91.79%) | Memory: 10.1 MB (13.81%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -1,23 +1,23 @@
 //Striver SDE Sheet Day 26
-//Aug'7,2023 06:40 pm
+//Aug'7,2023 06:50 pm
 
 class Solution {
-private:
-    int f(int i, int j, vector<vector<int>>&grid, vector<vector<int>>&dp){
-        if(i==grid.size()-1 && j==grid[0].size()-1){
-            return grid[i][j];
-        }
-        if(i==grid.size() || j==grid[0].size())return 1e8;
-
-        if(dp[i][j]!=-1)return dp[i][j];
-        int right= grid[i][j]+ f(i, j+1, grid, dp);
-        int down= grid[i][j]+ f(i+1, j, grid, dp);
-
-        return dp[i][j]=min(right, down);
-    }
 public:
     int minPathSum(vector<vector<int>>& grid) {
-        vector<vector<int>>dp(grid.size()+1, vector<int>(grid[0].size()+1, -1));
-        return f(0, 0, grid, dp);
+        int n=grid.size(), m=grid[0].size();
+        vector<vector<int>>dp(n+2, vector<int>(m+2, 0));
+        for(int i=0;i<=n;i++)dp[i][m]=1e8;
+        for(int j=0;j<=m;j++)dp[n][j]=1e8;
+        dp[n][m]=0;
+        dp[n-1][m]=0;
+        dp[n][m-1]=0;
+        for(int i=n-1; i>=0; i--){
+            for(int j=m-1; j>=0; j--){
+                int right= grid[i][j]+ dp[i][j+1];
+                int down= grid[i][j]+ dp[i+1][j];
+                dp[i][j]=min(right, down);
+            }
+        }
+        return dp[0][0];
     }
 };
