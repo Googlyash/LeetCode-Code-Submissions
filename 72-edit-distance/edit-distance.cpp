dfcbcb4f9807d71c03740a603088a8a190fd37b1
Googlyash <77059584+Googlyash@users.noreply.github.com>
2023-08-07 18:26:32 +0530

    Time: 13 ms (65.07%) | Memory: 9 MB (61.04%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,26 +1,30 @@
 //Aug'7,2023 06:20 pm
 //Self 
-//Recursion with memoization
+//Tabulation
 
 class Solution {
-private:
-    int f(int i, int j, string &s1, string &s2, vector<vector<int>>&dp){
-        if(j==0)return i;
-        if(i==0)return j;
-
-        if(dp[i][j]!=-1)return dp[i][j];
-        int take= 1e9;
-        if(s1[i-1]==s2[j-1]){
-            take= 0+f(i-1, j-1, s1, s2, dp);
-        }
-        int nottake= 1+ min(f(i-1, j-1, s1, s2, dp), min(f(i, j-1, s1, s2, dp), f(i-1, j, s1, s2, dp)));
-        return dp[i][j]=min(take, nottake);
-    }
 public:
     int minDistance(string s1, string s2) {
         int n=s1.size(), m=s2.size();
-        vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
-        return f(n, m, s1, s2, dp);
+        vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
+
+        for(int i=0;i<=n;i++){
+            dp[i][0]=i;
+        }
+        for(int j=0;j<=m;j++){
+            dp[0][j]=j;
+        }
+        for(int i=1;i<=n; i++){
+            for(int j=1;j<=m; j++){
+                int take= 1e8;
+                if(s1[i-1]==s2[j-1]){
+                    take= 0+dp[i-1][j-1];
+                }
+                int nottake= 1+ min(dp[i-1][j-1], min(dp[i][j-1], dp[i-1][j]));
+                dp[i][j]=min(take, nottake);
+            }
+        }
+        return dp[n][m];
     }
 };
 
