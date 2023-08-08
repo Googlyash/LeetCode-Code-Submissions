779108c2569cc876150a44af6004fb6a65daf7e3
Googlyash <77059584+Googlyash@users.noreply.github.com>
2023-08-09 01:05:33 +0530

    Time: 97 ms (81.08%) | Memory: 67.8 MB (32.65%) - LeetSync

---
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -0,0 +1,17 @@
+//Striver SDE Sheet
+//Kadene Algo
+//Aug'9, 2023 01:02 am
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& a) {
+        int n=a.size();
+        int mx=a[0], cur=a[0];
+        for(int i=1;i<n;i++){
+            cur=max(a[i], cur+a[i]);
+            mx=max(cur, mx);
+            if(cur<0)cur=0;
+        }
+        return mx;
+    }
+};
