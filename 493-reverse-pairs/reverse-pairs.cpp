d4dc70822d94e665d1c691fac487d6ab38e9683c
Googlyash <77059584+Googlyash@users.noreply.github.com>
2023-08-09 21:07:59 +0530

    Time: 667 ms (59.62%) | Memory: 237.5 MB (16.29%) - LeetSync

--- 493-reverse-pairs/reverse-pairs.cpp
+++ 493-reverse-pairs/reverse-pairs.cpp
@@ -1,6 +1,6 @@
 //Striver SDE Sheet: day 3
 //Striver's Code
-//Aug'9, 2023 08:47 pm
+//Aug'9, 2023 09:07 pm
 
 class Solution {
 private:
@@ -28,23 +28,19 @@ private:
             arr[i]=temp[i-l];
         }
     }
-    long long cntHelper(int l, int mid, int r, vector<int>&arr){
-        int rt=mid+1;
-        long long cnt=0;
+    int cntHelper(int l, int mid, int r, vector<int>&arr){
+        int rt=mid+1, mx=INT_MAX, mn=INT_MIN;
+        int cnt=0;
         for(int i=l;i<=mid; i++){
-            long temp=arr[i];
-            long cur=0;
-            if(rt<=r)cur=arr[rt];
-            while(rt<=r && temp>cur*2){
+            while(rt<=r && arr[i]>2L*arr[rt]){
                 rt++;
-                if(rt<=r)cur=arr[rt];
             }
             cnt+=rt-mid-1;
         }
         return cnt;
     }
-    long long merge(int l, int r, vector<int>&nums){
-        long long cnt=0;
+    int merge(int l, int r, vector<int>&nums){
+        int cnt=0;
         if(l>=r){
             return cnt;
         }
@@ -59,7 +55,7 @@ private:
     }
 public:
     int reversePairs(vector<int>& nums) {
-        int n=nums.size(), mod=1e9+7;
-        return merge(0, n-1, nums)%mod;
+        int n=nums.size();
+        return merge(0, n-1, nums);
     }
 };
