a58c14e8fc60014032bdae9fb59e4fd920510bac
Googlyash <77059584+Googlyash@users.noreply.github.com>
2023-08-11 21:37:33 +0530

    Time: 38 ms (44.07%) | Memory: 18.6 MB (37.65%) - LeetSync

---
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -0,0 +1,24 @@
+//POTD Aug'11, 2023 09:36 pm
+
+class Solution {
+public:
+    int change(int amount, vector<int>& coins) {
+        int n=coins.size();
+        vector<vector<int>>dp(n+1, vector<int>(amount+1,0));
+        for(int i=0;i<=n;i++){
+            dp[i][0]=1;
+        }
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=amount; j++){
+                int nottake=dp[i-1][j];
+                int take=0;
+                if(coins[i-1]<=j){
+                    take=dp[i][j-coins[i-1]];
+                }
+                dp[i][j]=take+nottake;
+            }
+        }
+        return dp[n][amount];
+    }
+};
