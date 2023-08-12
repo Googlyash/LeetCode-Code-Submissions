57ede8b17c264c33f0d0a65ca059edf28733ddda
Googlyash <77059584+Googlyash@users.noreply.github.com>
2023-08-12 18:24:12 +0530

    Time: 2 ms (68.17%) | Memory: 7.6 MB (81.25%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -1,26 +1,27 @@
 //POTD Aug'12, 2023 06:14 pm
+//Spae Optimised
 
 class Solution {
 public:
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         int n=obstacleGrid.size(), m=obstacleGrid[0].size();
-        vector<vector<int>>dp(n+1, vector<int>(m+1, 0));
-        if(obstacleGrid[0][0]==0)dp[0][0]=1;
-        for(int i=1;i<n;i++){
-            dp[i][0]=dp[i-1][0];
-            if(obstacleGrid[i][0]==1)dp[i][0]=0;
-        }
+        
+        vector<int>pre(m+1, 0), cur(m+1, 0);
+        if(obstacleGrid[0][0]==0)pre[0]=1;
         for(int j=1;j<m;j++){
-            dp[0][j]=dp[0][j-1];
-            if(obstacleGrid[0][j]==1)dp[0][j]=0;
+            pre[j]=pre[j-1];
+            if(obstacleGrid[0][j]==1)pre[j]=0;
         }
 
         for(int i=1;i<n;i++){
+            cur[0]=pre[0];
+            if(obstacleGrid[i][0]==1)cur[0]=0;
             for(int j=1;j<m;j++){
-                dp[i][j]=dp[i-1][j]+dp[i][j-1];
-                if(obstacleGrid[i][j]==1)dp[i][j]=0;
+                cur[j]=pre[j]+cur[j-1];
+                if(obstacleGrid[i][j]==1)cur[j]=0;
             }
+            pre=cur;
         }
-        return dp[n-1][m-1];
+        return pre[m-1];
     }
 };
