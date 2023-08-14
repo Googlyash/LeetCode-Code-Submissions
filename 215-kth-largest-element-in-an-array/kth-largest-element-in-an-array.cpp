81d71d82d89787f7850ddc833f1d5ba517f3a4e3
Googlyash <77059584+Googlyash@users.noreply.github.com>
2023-08-14 11:21:56 +0530

    Time: 56 ms (99.43%) | Memory: 67.1 MB (5.24%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
@@ -1,18 +1,42 @@
 //POTD Aug'14, 2023
 //Aug'14,2023 11:09 am
+//Quicksort: Editorial
 
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
-        int n=nums.size();
-        priority_queue<int, vector<int>, greater<int>> pq;
-        for(int i=0;i<n;i++){
-            pq.push(nums[i]);
-            if(pq.size()>k)pq.pop();
+        return quickSelect(nums, k);
+    }
+    
+    int quickSelect(vector<int>& nums, int k) {
+        int pivot = nums[rand() % nums.size()];
+        
+        vector<int> left;
+        vector<int> mid;
+        vector<int> right;
+        
+        for (int num: nums) {
+            if (num > pivot) {
+                left.push_back(num);
+            } else if (num < pivot) {
+                right.push_back(num);
+            } else {
+                mid.push_back(num);
+            }
         }
-        return pq.top();
+        
+        if (k <= left.size()) {
+            return quickSelect(left, k);
+        }
+        
+        if (left.size() + mid.size() < k) {
+            return quickSelect(right, k - left.size() - mid.size());
+        }
+        
+        return pivot;
+        
     }
 };
 
-//Time Complexity: O(n*log(k));
-//Space complexity: O(k)
+//Time Complexity: O(n), Worst O(n^2);
+//Space complexity: O(n)
