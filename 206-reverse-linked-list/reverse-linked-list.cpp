8530e36e6294c5fd96756e23f79982b2c44a327a
Googlyash <77059584+Googlyash@users.noreply.github.com>
2023-08-14 22:21:23 +0530

    Time: 2 ms (94.95%) | Memory: 8.4 MB (28.77%) - LeetSync

---
+++ 206-reverse-linked-list/reverse-linked-list.cpp
@@ -0,0 +1,33 @@
+//Striver SDE Sheet: Day 5
+//AAug'14, 2023 10:20 pm
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if(head==NULL)return head;
+        ListNode* temp=head;
+        vector<int>v;
+        while(temp!=NULL){
+            v.push_back(temp->val);
+            temp=temp->next;
+        }
+        temp=head;
+        reverse(v.begin(), v.end());
+        int i=0;
+        while(temp!=NULL){
+            temp->val=v[i++];
+            temp=temp->next;
+        }
+        return head;
+    }
+};
