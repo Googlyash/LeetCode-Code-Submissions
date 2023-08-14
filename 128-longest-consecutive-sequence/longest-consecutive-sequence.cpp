2889ab05408ef6097f7bfcd9db6eb8370c344690
Googlyash <77059584+Googlyash@users.noreply.github.com>
2023-08-14 11:41:29 +0530

    Time: 128 ms (71.52%) | Memory: 60.9 MB (64.94%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -1,17 +1,22 @@
 //Striver SDE Sheet day 4
-//Aug'13, 2023 07:33 pm
+//Aug'14, 2023 11:40 pm
+//Striver's Solution
 
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        int n=nums.size(), mx=0;
-        map<int, int>mp;
-        for(int i=0;i<n;i++){
-            mp[nums[i]]=mp[nums[i]-1]+1;
-        }
-        for(auto it:mp){
-            mx=max(mx, it.second);
+        int n=nums.size();
+        int mx=0;
+        unordered_set<int>st(nums.begin(), nums.end());
+        for(auto it:st){
+            if(st.find(it-1)==st.end()){
+                int cnt=1;
+                int cur=it;
+                while(st.find(++cur)!=st.end()){
+                    cnt++;
+                }
+                mx=max(mx, cnt);
+            }
         }
         return mx;
     }
