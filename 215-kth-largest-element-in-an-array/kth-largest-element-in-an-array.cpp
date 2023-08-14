b6378d05a87b384ce04333ef464d1786a0d5c812
Googlyash <77059584+Googlyash@users.noreply.github.com>
2023-08-14 11:16:03 +0530

    Time: 81 ms (91.37%) | Memory: 47.3 MB (49.46%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
@@ -8,8 +8,10 @@ public:
         priority_queue<int, vector<int>, greater<int>> pq;
         for(int i=0;i<n;i++){
             pq.push(nums[i]);
+            if(pq.size()>k)pq.pop();
         }
-        while(pq.size()!=k) pq.pop();
         return pq.top();
     }
 };
+
+//Time Complexity: O(n*log(k));
