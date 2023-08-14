9afb659265d84710e37f87ec7dc29595bce58b8a
Googlyash <77059584+Googlyash@users.noreply.github.com>
2023-08-14 23:52:11 +0530

    Time: 5 ms (91.62%) | Memory: 7.8 MB (15.49%) - LeetSync

---
+++ 237-delete-node-in-a-linked-list/delete-node-in-a-linked-list.cpp
@@ -0,0 +1,17 @@
+//Striver SDE Sheet: Day 5
+//Aug'14, 2023 11:51 pm
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        node->val=node->next->val;
+        node->next=node->next->next;
+    }
+};
