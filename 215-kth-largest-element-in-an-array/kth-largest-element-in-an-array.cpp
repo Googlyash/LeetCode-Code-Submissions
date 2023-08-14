45098a37aca492ed8335ccf55cf307d5096a586f
Googlyash <77059584+Googlyash@users.noreply.github.com>
2023-08-14 11:10:39 +0530

    Time: 100 ms (74.70%) | Memory: 49.9 MB (11.09%) - LeetSync

---
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
@@ -0,0 +1,15 @@
+//POTD Aug'14, 2023
+//Aug'14,2023 11:09 am
+
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        int n=nums.size();
+        priority_queue<int, vector<int>, greater<int>> pq;
+        for(int i=0;i<n;i++){
+            pq.push(nums[i]);
+        }
+        while(pq.size()!=k) pq.pop();
+        return pq.top();
+    }
+};
