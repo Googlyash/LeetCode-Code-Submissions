015b0e06e47af26e304f5065bcc5baa87857c9d9
Googlyash <77059584+Googlyash@users.noreply.github.com>
2023-08-14 11:26:48 +0530

    Time: 108 ms (70.04%) | Memory: 47.2 MB (66.20%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
@@ -1,41 +1,18 @@
 //POTD Aug'14, 2023
-//Aug'14,2023 11:25 am
-//CountingSort: Editorial
+//Aug'14,2023 11:26 am
 
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
-        int minValue = INT_MAX;
-        int maxValue = INT_MIN;
-        
-        for (int num: nums) {
-            minValue = min(minValue, num);
-            maxValue = max(maxValue, num);
+        int n=nums.size();
+        priority_queue<int, vector<int>, greater<int>> pq;
+        for(int i=0;i<n;i++){
+            pq.push(nums[i]);
+            if(pq.size()>k)pq.pop();
         }
-        
-        vector<int> count(maxValue - minValue + 1);
-        for (int num: nums) {
-            count[num - minValue]++;
-        }
-        
-        int remain = k;
-        for (int num = count.size() - 1; num >= 0; num--) {
-            remain -= count[num];
-            if (remain <= 0) {
-                return num + minValue;
-            }
-        }
-        
-        return -1;
+        return pq.top();
     }
 };
 
-/*Given n as the length of nums and mmm as maxValue - minValue,
-Time complexity: O(n+m)
-We first find maxValue and minValue, which costs O(n)
-Next, we initialize count, which costs O(m)
-Next, we populate count, which costs O(n)
-Finally, we iterate over the indices of count, which costs up to O(m)
-
-Space complexity: O(m)
-We create an array count with size O(m)*/
+//Time Complexity: O(n*log(k));
+//Space complexity: O(k)
