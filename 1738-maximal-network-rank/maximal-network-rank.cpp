7cff4e67e3e316792a271340e3eb3f8e40815109
Googlyash <77059584+Googlyash@users.noreply.github.com>
2023-08-19 02:16:23 +0530

    Time: 99 ms (50.00%) | Memory: 39 MB (37.17%) - LeetSync

---
+++ 1738-maximal-network-rank/maximal-network-rank.cpp
@@ -0,0 +1,32 @@
+//POTD Aug'18, 2023
+//Aug'19, 2023 2:15 am
+
+class Solution {
+public:
+    int maximalNetworkRank(int n, vector<vector<int>>& roads) {
+        vector<int> degree(n, 0);
+        set<pair<int, int>> road_set;
+        
+        for (auto& road : roads) {
+            degree[road[0]]++;
+            degree[road[1]]++;
+            road_set.insert({road[0], road[1]});
+            road_set.insert({road[1], road[0]});
+        }
+
+        int max_rank = 0;
+        for (int i = 0; i < n; ++i) {
+            for (int j = i+1; j < n; ++j) {
+                int rank = degree[i] + degree[j];
+                if (road_set.find({i, j}) != road_set.end()) {
+                    rank--;
+                }
+                max_rank = max(max_rank, rank);
+            }
+        }
+
+        return max_rank;
+    }
+};
+
+//Comments
