7ab9d58a03ae5ff8e3d388bfa306a7c73d2b8fbb
Googlyash <77059584+Googlyash@users.noreply.github.com>
2023-08-29 11:06:25 +0530

    Time: 89 ms (60.37%) | Memory: 48.2 MB (18.49%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -6,14 +6,12 @@ public:
         int n=nums.size();
         int ans=0;
         vector<int>pre(n+1, 0);
-        map<int, int>mp;
+        unordered_map<int, int>mp;
         mp[0]=1;
         for(int i=0;i<n;i++){
             pre[i+1]=pre[i]+nums[i];
             ans+=mp[pre[i+1]-k];
             mp[pre[i+1]]++;
-            // mp[pre[i+1]]+=mp[pre[i+1]-k];
-            cout<<mp[pre[i+1]-k]<<endl;
         }
         return ans;
     }
