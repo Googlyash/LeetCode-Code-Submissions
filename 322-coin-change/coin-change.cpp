efda57e37baf0e9da5a8e1c46e3401861ef2d865
Googlyash <77059584+Googlyash@users.noreply.github.com>
2023-08-29 15:18:08 +0530

    Time: 95 ms (54.82%) | Memory: 50.3 MB (7.38%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,27 +1,49 @@
-//Aug'7, 2023 07: pm
-//Recursion with memoization
+//Aug'29, 2023 03:17 pm
 
 class Solution {
-private:
-    int f(int i, int tot, vector<int>&a, vector<vector<int>>&dp){
-        if(tot==0)return 0;
-        if(i<0 || tot<0)return 1e8;
-
-        if(dp[i][tot]!=-1) return dp[i][tot];
-        int nottake= 0+ f(i-1, tot, a, dp);
-        int take=1e9, mod=1e9+7, temp=tot;
-        if(a[i]<=temp ){
-            temp-=a[i];
-            take= min(take, 1+(f(i, temp, a, dp))%mod);
+private: 
+    int solve(int i, int cnt, vector<int>&v, vector<vector<int>>&dp){
+        if(i<=0 && cnt!=0){
+            return 1e8;
         }
-        return dp[i][tot]=min( take, nottake);
+        if(cnt==0){
+            return 0;
+        }
+
+        if(dp[i][cnt]!=-1){
+            return dp[i][cnt];
+        }
+        int nottake=solve(i-1, cnt, v, dp);
+        int take=1e8;
+        if(v[i-1]<=cnt){
+            take= 1+solve(i, cnt-v[i-1], v, dp);
+        }
+
+        return dp[i][cnt]=min(take, nottake);
     }
 public:
     int coinChange(vector<int>& coins, int amount) {
         int n=coins.size();
         vector<vector<int>>dp(n+1, vector<int>(amount+1, -1));
-        int ans=f(n-1, amount, coins, dp);
-        if(ans>= 1e8)return -1;
-        return  ans;
+        int ans=solve(n, amount, coins, dp);
+        if(ans>=1e8)return -1;
+        return ans;
+
+        
+        // vector<vector<int>>dp(n+1, vector<int>(amount+1, 0));
+        for(int cnt=1;cnt<=amount; cnt++){
+            dp[0][cnt]=1e8;
+        }
+        for(int i=1;i<=n;i++){
+            for(int cnt=0;cnt<=amount;cnt++){
+                int nottake=dp[i-1][cnt];
+                int take=1e8;
+                if(coins[i-1]<=cnt){
+                    take= 1+ dp[i][cnt-coins[i-1]];
+                }
+                dp[i][cnt]=min(take, nottake);
+            }
+        }
+        return dp[n][amount]>-1e8? -1: dp[n][amount];
     }
 };
