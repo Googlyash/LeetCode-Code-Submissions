fa6f3721bb85a94615f1764f2918b8b88beb5e73
Googlyash <77059584+Googlyash@users.noreply.github.com>
2023-09-01 09:20:06 +0530

    Time: 3 ms (89.82%) | Memory: 8.4 MB (32.26%) - LeetSync

---
+++ 338-counting-bits/counting-bits.cpp
@@ -0,0 +1,21 @@
+//POTD Sep'1, 2023
+//Sep'1, 2023 09:19 am
+
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int>v;
+        for(int i=0;i<=n;i++)
+        {
+            int ans=0;
+            int j=i;
+            while(i){
+                ans+=i%2;
+                i/=2;
+            }
+            i=j;
+            v.push_back(ans);
+        }
+        return v;
+    }
+};
