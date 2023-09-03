2eec66f449f8ed0cd0202f5f28cce1f88965722f
Googlyash <77059584+Googlyash@users.noreply.github.com>
2023-09-03 11:47:09 +0530

    Time: 0 ms (100.00%) | Memory: 5.9 MB (88.67%) - LeetSync

---
+++ 62-unique-paths/unique-paths.cpp
@@ -0,0 +1,37 @@
+//POTD Sep'3, 2023
+//Striver day3
+//Striver- O(n) space and O(n) Time
+//Earlier Self Solved
+
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        long long ans=1, mod=1e9+7;
+        // n--, m--;
+        for(int i=n;i<m+n-1;i++){
+            ans*=(i);
+            ans/=(i-n+1);
+            // ans%=mod;
+        }
+        return ans;
+        // vector<int>dp(n,0);
+        // for(int i=0;i<m;i++){
+        //     vector<int>curr(n,0);
+        //     for(int j=0;j<n;j++){
+        //         if(i==0 || j==0)curr[j]=1;
+        //         else {
+        //             curr[j]=curr[j-1]+dp[j];
+        //         }
+        //     }
+        //     dp=curr;
+        // }
+        // for(int i=0;i<m;i++)dp[i][0]=1;
+        // for(int i=0;i<n;i++)dp[0][i]=1;
+        // for(int i=1;i<m;i++){
+        //     for(int j=1;j<n;j++){
+        //         dp[i][j]=dp[i-1][j]+dp[i][j-1];
+        //     }
+        // }
+        // return dp[n-1];
+    }
+};
