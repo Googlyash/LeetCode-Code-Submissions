52dd44441b48603cf1d0e95f9c95c3c77e3bd771
Googlyash <77059584+Googlyash@users.noreply.github.com>
2023-09-03 14:38:12 +0530

    Time: 8 ms (80.00%) | Memory: 6.2 MB (100.00%) - LeetSync

---
+++ 3046-minimum-operations-to-make-a-special-number/minimum-operations-to-make-a-special-number.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int minimumOperations(string s) {
+        int n=s.size(),ans=n;
+        int z1=n, f=n+1, cnt=0;
+
+        for(int i=n-1; i>=0; i--){
+            if(s[i]=='0' && z1==n){
+                z1=i;
+            }
+            else if((s[i]=='0' || s[i]=='5') && z1<n){
+                f=n-1-i;
+                break;
+            }
+        }
+        ans=min(ans, f-1);
+        z1=n, f=n+1;
+        for(int i=n-1; i>=0; i--){
+            if(s[i]=='5' && z1==n){
+                z1=i;
+            }
+            else if((s[i]=='2' || s[i]=='7') && z1<n){
+                f=n-1-i;
+                break;
+            }
+        }
+        ans=min(ans, f-1);
+
+        for(int i=0;i<n;i++){
+            if(s[i]=='0')cnt++;
+        }
+        cout<<ans<<endl;
+        ans=min(ans, n-cnt);
+        cout<<ans<<endl;
+        return ans;
+    }
+};
