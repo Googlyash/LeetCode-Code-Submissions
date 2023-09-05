b20f46bc3c8fa44f856aed231288470ab7366468
Googlyash <77059584+Googlyash@users.noreply.github.com>
2023-09-05 18:00:27 +0530

    Time: 7 ms (71.12%) | Memory: 11.5 MB (8.55%) - LeetSync

---
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
@@ -0,0 +1,59 @@
+//POTD Sep'5, 2023
+//Sep'5, 2023 05:58 pm
+
+
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+    
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+*/
+
+class Solution {
+public:
+    Node* copyRandomList(Node* head) {
+        if(head==NULL)return head;
+        Node* newnode= new Node(head->val);
+        Node*temp=head;
+        Node*main=newnode;
+        map<Node*, Node*>mp, mp1;
+        // int i=0;
+        while(temp!=NULL && temp->next!=NULL){
+            Node*cur= new Node(0);
+            main->val=temp->val;
+            if(temp->next)cur->val=temp->next->val;
+            main->next=cur;
+            mp[temp]=temp->random;
+            main=main->next;
+            temp=temp->next;
+        }
+        temp=head;
+        main=newnode;
+        while(temp!=NULL){
+            if(temp->random==NULL)main->random=temp->random;
+            else {
+                Node*randomTemp=head, *randomcur=newnode;
+                while(randomTemp!=NULL){
+                    if(temp->random==randomTemp){
+                        main->random=randomcur;
+                        break;
+                    }
+                    randomTemp=randomTemp->next;
+                    randomcur=randomcur->next;
+                }
+            }
+            temp=temp->next;
+            main=main->next;
+        }
+        return newnode;
+    }
+};
