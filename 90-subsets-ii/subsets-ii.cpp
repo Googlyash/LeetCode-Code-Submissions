c13b29a87ca25c1045a609c5a7039b07f101168d
Googlyash <77059584+Googlyash@users.noreply.github.com>
2023-09-09 21:10:09 +0530

    Time: 4 ms (53.29%) | Memory: 7.4 MB (77.20%) - LeetSync

--- 90-subsets-ii/subsets-ii.cpp
+++ 90-subsets-ii/subsets-ii.cpp
@@ -3,29 +3,22 @@
 
 class Solution {
 private:
-    void solve(int i, vector<int>&v, vector<vector<int>>&ans, vector<int>&nums){
-        if(i<0){
-            ans.push_back(v);
-            return ;
+    void solve(int idx, vector<int>&v, vector<vector<int>>&ans, vector<int>&nums){
+        ans.push_back(v);
+        for(int i=idx; i<nums.size(); i++){
+            if(i!=idx && nums[i]==nums[i-1])continue;
+            v.push_back(nums[i]);
+            solve(i+1, v, ans, nums);
+            v.pop_back();
         }
-
-        //notTake
-        solve(i-1, v, ans, nums);
-
-        //Take
-        v.push_back(nums[i]);
-        solve(i-1, v, ans, nums);
-        v.pop_back();
     }
 public:
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         sort(nums.begin(), nums.end());
         vector<vector<int>>ans;
         vector<int>v;
-        solve(nums.size()-1, v, ans,nums);
+        solve(0, v, ans,nums);
         sort(ans.begin(), ans.end());
-        set<vector<int>>st(ans.begin(), ans.end());
-        vector<vector<int>>distinctNums(st.begin(), st.end());
-        return distinctNums;
+        return ans;
     }
 };
