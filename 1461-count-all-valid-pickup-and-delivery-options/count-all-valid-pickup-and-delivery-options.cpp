68cacef0412d5b45a38f061af622f6a87258cb6b
Googlyash <77059584+Googlyash@users.noreply.github.com>
2023-09-10 23:03:18 +0530

    Time: 0 ms (100.00%) | Memory: 6 MB (43.33%) - LeetSync

---
+++ 1461-count-all-valid-pickup-and-delivery-options/count-all-valid-pickup-and-delivery-options.cpp
@@ -0,0 +1,17 @@
+//POTD Sep'10, 2023
+
+class Solution {
+public:
+    int countOrders(int n) {
+        long long ans=1, mod=1e9+7;
+        for(int i=1;i<=n;i++){
+            ans*=i;
+            ans%=mod;
+        }
+        for(int i=1;i<2*n;i+=2){
+            ans*=i;
+            ans%=mod;
+        }
+        return ans;
+    }
+};
