b2cd5e865b1e3df0c8437ac1443fb870e8d2108e
Googlyash <77059584+Googlyash@users.noreply.github.com>
2023-09-11 15:48:31 +0530

    Time: 4 ms (62.44%) | Memory: 10.6 MB (78.70%) - LeetSync

---
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -0,0 +1,35 @@
+//Sep'9, 2023 10:07pm
+//BackTracking Practice
+//Almost self-solved
+//Comments
+
+class Solution {
+private:
+    void solve(int idx, int remainingSum, vector<int>&v, vector<vector<int>>&ans, vector<int>&candidates){
+        if(remainingSum==0){
+            ans.push_back(v);
+            return ;
+        }
+
+        int x= idx;
+        while(x<candidates.size() && candidates[idx]<= remainingSum){
+            if(x>idx && candidates[x]== candidates[x-1]){
+                x++;
+                continue;
+            }
+
+            v.push_back(candidates[x]);
+            solve(x+1, remainingSum-candidates[x],  v, ans, candidates);
+            v.pop_back();
+            x++;
+        }
+    }
+public:
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        sort(candidates.begin(), candidates.end());
+        vector<vector<int>>ans;
+        vector<int>v;
+        solve(0, target, v, ans, candidates);
+        return ans;
+    }
+};
