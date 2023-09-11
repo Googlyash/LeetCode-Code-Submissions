048bade2b5eba67980ef8bc9ba4bc8a5e6a0f80d
Googlyash <77059584+Googlyash@users.noreply.github.com>
2023-09-11 17:16:48 +0530

    Time: 102 ms (64.39%) | Memory: 58.1 MB (64.20%) - LeetSync

--- 1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.cpp
+++ 1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.cpp
@@ -1,35 +1,22 @@
-//Sep'11, 2023 05:13 pm
+//Sep'11, 2023 05:15 pm
+//Earlier Self-Solved
+//Comments- Better Time Complexity
 
 class Solution {
 public:
     int maxOperations(vector<int>& nums, int k) {
         int n=nums.size();
-        if(n==1)return 0;
-        // sort(nums.begin(), nums.end());
+        sort(nums.begin(), nums.end());
         int l=0, r=n-1, ans=0;
-        map<int, int>mp, mp1;
-        for(int i=0;i<n;i++){
-            mp[nums[i]]++;
-        }
-        for(int i=0;i<n;i++){
-            if(nums[i]!= k-nums[i])
-                ans+= min(mp[nums[i]], mp[k-nums[i]]);
-            else {
-                ans+=mp[nums[i]]/2;
+        while(l<r){
+            if(nums[l]+nums[r]==k){
+                ans++;
+                l++;
+                r--;
             }
-            mp[nums[i]]=0;
-            cout<<ans<<" ";
+            else if(nums[l]+nums[r]>k )r--;
+            else l++;
         }
-        // while(l<n && l<r){
-        //     while(r>l && nums[l]+nums[r]>k){
-        //         r--;
-        //     }
-        //     if(nums[l]+nums[r]==k){
-        //         ans++;
-        //     }
-        //     r--;
-        //     l++;
-        // }
         return ans;
     }
 };
