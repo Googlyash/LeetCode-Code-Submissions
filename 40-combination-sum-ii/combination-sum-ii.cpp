8a8b89e0454fe3e54088af6cd006c2878524ef79
Googlyash <77059584+Googlyash@users.noreply.github.com>
2023-09-11 15:56:00 +0530

    Time: 7 ms (44.87%) | Memory: 10.7 MB (53.19%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.cpp
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -1,6 +1,6 @@
-//Sep'9, 2023 10:07pm
+//Sep'1, 2023 3:55pm
 //BackTracking Practice
-//Almost self-solved
+//Almost Self-solved
 //Comments
 
 class Solution {
@@ -10,16 +10,16 @@ private:
             ans.push_back(v);
             return ;
         }
-
         int x= idx;
-        while(x<candidates.size() && candidates[idx]<= remainingSum){
-            if(x>idx && candidates[x]== candidates[x-1]){
+
+        while(x< candidates.size() && remainingSum>= candidates[idx]){
+            if(x> idx &&  candidates[x]== candidates[x-1]){
                 x++;
                 continue;
             }
 
             v.push_back(candidates[x]);
-            solve(x+1, remainingSum-candidates[x],  v, ans, candidates);
+            solve(x+1, remainingSum-candidates[x], v, ans, candidates);
             v.pop_back();
             x++;
         }
