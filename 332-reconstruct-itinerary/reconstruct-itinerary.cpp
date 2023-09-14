c48301530c0b05229c7ef89e7ae39820b7b5f9f8
Googlyash <77059584+Googlyash@users.noreply.github.com>
2023-09-14 18:43:59 +0530

    Time: 18 ms (76.95%) | Memory: 14.6 MB (55.27%) - LeetSync

--- 332-reconstruct-itinerary/reconstruct-itinerary.cpp
+++ 332-reconstruct-itinerary/reconstruct-itinerary.cpp
@@ -1,33 +1,32 @@
 //POTD Sep'14, 2023 
-//Sep'14, 2023 05:40 pm
+//Sep'14, 2023 06:40 pm
 
 
 class Solution {
 public:
     vector<string> findItinerary(vector<vector<string>>& tickets) {
         int n=tickets.size();
-        unordered_map<string, vector<string>>adj;
+        unordered_map<string, multiset<string>>adj;
+        stack<string>st;
 
         for(int i=0;i<n;i++){
-            adj[tickets[i][0]].push_back(tickets[i][1]);
+            adj[tickets[i][0]].insert(tickets[i][1]);
         }
-        for(auto& [_, dest]: adj){
-            sort(dest.rbegin(), dest.rend());
-        }
-        stack<string>st;
         st.push("JFK");
 
         vector<string>ans;
         while(!st.empty()){
             string src=st.top();
 
-            if(adj.find(src)!=adj.end() && !adj[src].empty()){
-                st.push(adj[src].back());
-                adj[src].pop_back();
-            }
-            else {
+            if(adj[src].size()==0){
                 ans.push_back(src);
                 st.pop();
+            }
+            else {
+                auto dest= (adj[src].begin());
+                st.push(*dest);
+                adj[src].erase(dest);
+
             }
         }
         reverse(ans.begin(), ans.end());
