30d3794aa01e5d7ed15d535f761a9a71d9e0fa09
Googlyash <77059584+Googlyash@users.noreply.github.com>
2023-09-14 18:35:39 +0530

    Time: 17 ms (80.19%) | Memory: 14.6 MB (58.51%) - LeetSync

---
+++ 332-reconstruct-itinerary/reconstruct-itinerary.cpp
@@ -0,0 +1,38 @@
+//POTD Sep'14, 2023 
+//Sep'14, 2023 05:40 pm
+
+
+class Solution {
+public:
+    vector<string> findItinerary(vector<vector<string>>& tickets) {
+        int n=tickets.size();
+        unordered_map<string, multiset<string>>adj;
+        stack<string>st;
+
+        for(int i=0;i<n;i++){
+            adj[tickets[i][0]].insert(tickets[i][1]);
+        }
+        st.push("JFK");
+
+        vector<string>ans;
+        while(!st.empty()){
+            string src=st.top();
+
+            if(adj[src].size()==0){
+                ans.push_back(src);
+                st.pop();
+            }
+            else {
+                auto dest= (adj[src].begin());
+                st.push(*dest);
+                adj[src].erase(dest);
+
+            }
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
+        return ans;
+    }
+};
+
+//Comments: 50 Min
