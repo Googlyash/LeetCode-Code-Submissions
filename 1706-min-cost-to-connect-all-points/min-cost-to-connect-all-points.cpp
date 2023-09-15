b8ac8bc4b2f4d8d737d2878b7bfc86f3fae81c70
Googlyash <77059584+Googlyash@users.noreply.github.com>
2023-09-15 18:27:36 +0530

    Time: 46 ms (99.79%) | Memory: 10.4 MB (92.71%) - LeetSync

--- 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.cpp
+++ 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.cpp
@@ -1,6 +1,6 @@
 //POTD Sep'15, 2023 
 //Sep'15, 2023 06:26 pm
-//Comments
+//Comments: Prism's Algo
 class Solution {
 public:
     int minCostConnectPoints(vector<vector<int>>& points) {
@@ -19,30 +19,8 @@ public:
             ans+=mn[mnj];
             i=mnj;
         }
-        // map<pair<pair<int, int>, pair<int, int>>, int>mp;
-        // for(int i=0;i<n;i++){
-        //     int mn=1e8;
-        //     int x1=points[i][0], y1=points[i][1];
-        //     pair<pair<int, int>, pair<int, int>>p= {{0,0}, {0,0}};
-        //     for(int j=0; j<n;j++){
-        //         if(j==i)continue;
-        //         int x2=points[j][0], y2=points[j][1];
-        //         if(mn> abs(x1-x2)+ abs(y1-y2)){
-        //             if((x1+y1> x2+y2) ){
-        //                 if(mn<1e8)mp[p]--;
-        //                 mp[{{x1,y1}, {x2,y2}}]++;
-        //                 mp[{{x2,y2}, {x1, y1}}]++;
-        //                 p={{x1,y1}, {x2,y2}};
-        //                 mn= abs(x1-x2)+ abs(y1-y2);
-        //             }
-        //         }
-        //     }
-        //     cout<<mn<<endl;
-        //     cout<<mp[p]<<endl;
-        //     cout<<p.first.first<<" "<<p.first.second<<" "<<p.second.first<<" "<<p.second.second<<endl;
-        //     cout<<endl;
-        //     if(mn<1e8 && (mp[p]<2))ans+=mn;
-        // }
         return ans;
     }
 };
+
+//Comments: 50 Min
