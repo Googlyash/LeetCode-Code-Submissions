08266d89b88aa56b71b48d2bbf6fc8d01db9c2cc
Googlyash <77059584+Googlyash@users.noreply.github.com>
2023-09-16 18:28:15 +0530

    Time: 119 ms (88.51%) | Memory: 42.8 MB (76.38%) - LeetSync

--- 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.cpp
+++ 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.cpp
@@ -6,35 +6,24 @@ public:
     int minCostConnectPoints(vector<vector<int>>& points) {
         int ans=0, n=points.size(), connected=0, i=0;
 
-        vector<pair<int, int> >adj[n];
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                int dist= abs(points[i][0]-points[j][0])+abs(points[j][1]- points[i][1]);
-                adj[i].push_back({j, dist});
-                adj[j].push_back({i, dist});
-            }
-        }
-        priority_queue<pair<int, int>, vector<pair<int,int>>, greater<pair<int, int>>>pq;
-        pq.push({0, 0});
-        vector<int>vis(n ,0);
-        while(!pq.empty()){
-            int node=pq.top().second;
-            int wt=pq.top().first;
-            pq.pop();
-
-            if(vis[node])continue;
-            vis[node]=1;
-            ans+=wt;
-            for(auto it:adj[node]){
-                int adjNode=it.first;
-                int adjW= it.second;
-                if(!vis[adjNode]){
-                    pq.push({adjW, adjNode});
+        vector<int>vis(n, 0);
+        priority_queue<pair<int, int>>pq;
+        while(++connected<n){
+            vis[i]=1;
+            for(int j=0;j<n;j++){
+                if(!vis[j]){
+                    pq.push({-(abs(points[i][0]-points[j][0])+abs(points[j][1]-points[i][1])), j});
                 }
             }
+            while(vis[pq.top().second]){
+                pq.pop();
+            }
+            ans-=pq.top().first;
+            i=pq.top().second;
+            pq.pop();
         }
         return ans;
     }
 };
 
-//Self+ Comments: 30 min
+//Comments: 30 min
