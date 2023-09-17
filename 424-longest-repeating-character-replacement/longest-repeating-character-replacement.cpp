8a309f46d14226d2d6354f1ec673c985ced42e27
Googlyash <77059584+Googlyash@users.noreply.github.com>
2023-09-17 17:45:56 +0530

    Time: 7 ms (80.65%) | Memory: 7.4 MB (9.44%) - LeetSync

---
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -0,0 +1,18 @@
+//Sep'17, 2023 05:44 pm
+
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        unordered_map<int, int>mp;
+        int l=0, c=0, ans=0;
+        for(int r=0;r<s.size(); r++){
+            mp[s[r]-'A']++;
+            c=max(c, mp[s[r]-'A']);
+            while(r-l+1-c> k){
+                mp[s[l++]-'A']--;
+            }
+            ans=max(ans, r-l+1);
+        }
+        return ans;
+    }
+};
