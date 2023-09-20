dacb810cbb936bf0ddf1780202bb148e2f00fb69
Googlyash <77059584+Googlyash@users.noreply.github.com>
2023-09-20 14:53:34 +0530

    Time: 129 ms (52.23%) | Memory: 98.8 MB (38.49%) - LeetSync

--- 1776-minimum-operations-to-reduce-x-to-zero/minimum-operations-to-reduce-x-to-zero.cpp
+++ 1776-minimum-operations-to-reduce-x-to-zero/minimum-operations-to-reduce-x-to-zero.cpp
@@ -3,26 +3,6 @@
 //Comments: 51min
 
 class Solution {
-// private:
-//     int solve(int l, int r, vector<int>&nums, int x, vector<int>&dpl, vector<int>&dpr){
-//         if(l>r){
-//             return (x==0)?0: 1e7;
-//         }
-//         if(x==0){
-//             return 0;
-//         }
-        
-//         if(nums[l]<=x ){
-//             if(dpl[l]!=-1)dpl[l]=min(1+ solve(l+1, r, nums, x-nums[l], dpl, dpr), dpl[l]);
-//             else dpl[l]=1+ solve(l+1, r, nums, x-nums[l], dpl, dpr);
-//         }
-//         if(nums[r]<=x){
-//            if(dpr[r]!=-1) dpr[r]=min(1+ solve(l, r-1, nums, x-nums[r], dpl, dpr), dpr[r]);
-//            else dpr[r]= 1+ solve(l, r-1, nums, x-nums[r], dpl, dpr);
-//         }
-
-//         return min(dpl[l], dpr[r]);
-//     }
 public:
     int minOperations(vector<int>& nums, int x) {
         int n=nums.size();
@@ -41,38 +21,6 @@ public:
             }
             r++;
         }
-        // while(l<=r && x>0){
-        //     if(nums[l]> nums[r]){
-        //         if(nums[l]<=x){
-        //             ans++;
-        //             x-=nums[l++];
-        //         }
-        //         else if(nums[r]<=x){
-        //             ans++;
-        //             x-=nums[r--];
-        //         }
-        //         else {
-        //             break;
-        //         }
-        //     }
-        //     else {
-        //         if(nums[r]<=x){
-        //             ans++;
-        //             x-=nums[r--];
-        //         }
-        //         else if(nums[l]<=x){
-        //             ans++;
-        //             x-=nums[l++];
-        //         }
-        //         else {
-        //             break;
-        //         }
-        //     }
-        //     cout<<"l= "<<l<<" r= "<<r<<" x= "<<x<<endl;
-        // }
         return (ans==-1)?-1: n-ans;
-        // if(x==0)return n+l-r-1;
-        // if(l>r)return -1;
-        // else return max(ans-2, -1);
     }
 };
