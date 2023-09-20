ac4244019339751473c2a0bced27b7c43ab0e199
Googlyash <77059584+Googlyash@users.noreply.github.com>
2023-09-20 15:42:17 +0530

    Time: 352 ms (17.87%) | Memory: 173 MB (7.56%) - LeetSync

--- 1776-minimum-operations-to-reduce-x-to-zero/minimum-operations-to-reduce-x-to-zero.cpp
+++ 1776-minimum-operations-to-reduce-x-to-zero/minimum-operations-to-reduce-x-to-zero.cpp
@@ -1,26 +1,38 @@
 //POTD Sep'20, 2023
 //Sep'20, 2023 02:52 pm
 //Comments: 51min
+//Second Method: map+prefix sum
+//O(n)
 
 class Solution {
 public:
     int minOperations(vector<int>& nums, int x) {
         int n=nums.size();
-        int l=0, r=0;
-        int ans=-1, sum=0, cursum=0;
+        int ans=1e7, sum=0, cursum=0;
         for(auto it:nums){
             sum+=it;
         }
-        while(r<n ){
-            cursum+=nums[r];
-            while(l<=r && cursum>sum-x){
-                cursum-=nums[l++];
-            }
-            if(cursum==sum-x){
-                ans=max(ans, r-l+1);
-            }
-            r++;
+        
+        unordered_map<int, int>mp;
+        mp[0]=0;
+        vector<int>pre(n+1, 0);
+        for(int i=0;i<n;i++){
+            pre[i+1]=pre[i]+nums[i];
         }
-        return (ans==-1)?-1: n-ans;
+        for(int i=0;i<n;i++){
+            mp[pre[i+1]]=i+1;
+        }
+        
+        if( mp.find(x)!=mp.end()){
+            ans=min(ans, mp[x]);
+        }
+        for(int i=n-1;i>=0; i--){
+            cursum+=nums[i];
+            int midsum=x-cursum;
+            if( mp.find(midsum)!=mp.end()){
+                ans=min(ans, mp[midsum]+n-i);
+            }
+        }
+        return (ans>n)?-1: ans;
     }
 };
