175f2d23aeda5319ff8a3d3c4d2732a15bb11e68
Googlyash <77059584+Googlyash@users.noreply.github.com>
2023-09-20 16:55:52 +0530

    Time: 485 ms (50.00%) | Memory: 175.4 MB (43.19%) - LeetSync

--- 1300-critical-connections-in-a-network/critical-connections-in-a-network.cpp
+++ 1300-critical-connections-in-a-network/critical-connections-in-a-network.cpp
@@ -1,43 +1,40 @@
-//Striver Graph: 55
-//Sep'5, 2023 07:24 pm
-//Striver's Code
+//Sep'20, 2023 04:53 pm
 
 class Solution {
 private:
     int timer=1;
-    void dfs(int node, int parent, int tin[], int low[], vector<int>&vis, vector<int>adj[], vector<vector<int>>&bridges){
-        vis[node]=1;
-        tin[node]=low[node]=timer;
-        timer++;
+private:
+    void dfs(int idx, int parent, vector<int>&vis, vector<int>&tn, vector<int>&low, vector<int>adj[], vector<vector<int>>&bridges){
+        vis[idx]=1;
+        tn[idx]=low[idx]=timer++;
 
-        for(auto it:adj[node]){
+        for(auto it:adj[idx]){
             if(it==parent)continue;
             if(!vis[it]){
-                dfs(it, node, tin, low, vis, adj, bridges);
-                low[node]=min(low[it], low[node]);
-                if(low[it]> tin[node]){
-                    bridges.push_back({node, it});
+                dfs(it, idx, vis, tn, low, adj, bridges);
+                low[idx]=min(low[idx], low[it]);
+                if(low[it]> tn[idx]){
+                    bridges.push_back({it, idx});
                 }
             }
-            else {
-                low[node]=min(low[it], low[node]);
+            else{
+                low[idx]=min(low[idx], low[it]);
             }
         }
     }
 public:
     vector<vector<int>> criticalConnections(int n, vector<vector<int>>& connections) {
-         vector<int>adj[n];
+        vector<int>adj[n];
         for(auto it:connections){
             adj[it[0]].push_back(it[1]);
             adj[it[1]].push_back(it[0]);
         }
-
+        vector<vector<int>> bridges;
+        vector<int>tn(n, 0);
+        vector<int>low(n+1, 0);
         vector<int>vis(n, 0);
-        int tin[n];
-        int low[n];
-        vector<vector<int>>bridges;
-        dfs(0, -1, tin, low, vis, adj, bridges);
+        vector<int>ans;
+        dfs(0, -1, vis, tn, low, adj, bridges);
         return bridges;
     }
 };
-
