fa437be57ea3dc0f66069814384067705bdc9457
Googlyash <77059584+Googlyash@users.noreply.github.com>
2023-09-30 14:13:26 +0530

    Time: 164 ms (7.00%) | Memory: 65.9 MB (5.02%) - LeetSync

--- 456-132-pattern/132-pattern.cpp
+++ 456-132-pattern/132-pattern.cpp
@@ -5,20 +5,36 @@ class Solution {
 public:
     bool find132pattern(vector<int>& nums) {
         int n=nums.size();
-        int mn=INT_MIN;
-        stack<int>st;
-        for(int i=n-1; i>=0; i--){
-            if(mn>nums[i]){
-                return true;
-            }
-            else {
-                while(!st.empty() && st.top()<nums[i]){
-                    mn=st.top();
-                    st.pop();
+        vector<int>pre(n+1, 1e9);
+        for(int i=0;i<n;i++){
+            pre[i+1]=min(pre[i], nums[i]);
+        }
+        set<int>st;
+        st.insert(nums[n-1]);
+        for(int i=n-2; i>=0;i--){
+            auto it=st.lower_bound(nums[i]);
+            if(it!=st.begin()){
+                it--;
+                if(pre[i]< *it){
+                    return true;
                 }
             }
-            st.push(nums[i]);
+            st.insert(nums[i]);
         }
+        // int mn=INT_MIN;
+        // stack<int>st;
+        // for(int i=n-1; i>=0; i--){
+        //     if(mn>nums[i]){
+        //         return true;
+        //     }
+        //     else {
+        //         while(!st.empty() && st.top()<nums[i]){
+        //             mn=st.top();
+        //             st.pop();
+        //         }
+        //     }
+        //     st.push(nums[i]);
+        // }
         return false;
     }
 };
