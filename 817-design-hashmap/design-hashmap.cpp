d9bf9d0ea299b2b6a4cce9daeef70866d5ff1a6d
Googlyash <77059584+Googlyash@users.noreply.github.com>
2023-10-04 16:47:26 +0530

    Time: 94 ms (93.13%) | Memory: 52.7 MB (75.68%) - LeetSync

--- 817-design-hashmap/design-hashmap.cpp
+++ 817-design-hashmap/design-hashmap.cpp
@@ -1,27 +1,55 @@
 //POTD Oct'4, 2023
 //Oct'4, 2023 03:22 pm
+//Fraz YouTube: https://www.youtube.com/watch?v=xVEGczCJCHs
 
 class MyHashMap {
-    vector<int>myArray;
+    vector<list<pair<int, int>>>mp;
+    int siz;
 public:
     MyHashMap() {
-        myArray= vector<int>(1e6+1, -1);
-
+        siz=100;
+        mp.resize(siz);
     }
     
+    int hash(int key){
+        return key%siz;
+    }
+    list<pair<int, int>>:: iterator search(int key){
+        int i=hash(key);
+        auto it=mp[i].begin();
+        while(it!=mp[i].end()){
+            if(it->first==key){
+                return it;
+            }
+            it++;
+        }
+        return it;
+    }
     void put(int key, int value) {
-        myArray[key]=value;
+        remove(key);
+        int i=hash(key);
+        mp[i].push_back({key, value});
     }
     
     int get(int key) {
-        return myArray[key];
+        int i=hash(key);
+        if(search(key)==mp[i].end()){
+            return -1;
+        }
+        return search(key)->second;
     }
     
     void remove(int key) {
-        myArray[key]=-1;
+        int i=hash(key);
+        if(get(key)==-1){
+            return ;
+        }
+        mp[i].erase(search(key));
     }
 };
 
+//This one is optimal one
+//Self Done Earlier: By Brute Force
 /**
  * Your MyHashMap object will be instantiated and called as such:
  * MyHashMap* obj = new MyHashMap();
