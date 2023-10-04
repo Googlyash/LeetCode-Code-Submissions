26c41ac410757b006c81e68c4c5d91746dab0549
Googlyash <77059584+Googlyash@users.noreply.github.com>
2023-10-04 18:40:45 +0530

    Time: 3 ms (47.57%) | Memory: 6.8 MB (30.33%) - LeetSync

--- 91-decode-ways/decode-ways.cpp
+++ 91-decode-ways/decode-ways.cpp
@@ -1,53 +1,46 @@
 //Oct'4, 2023 06:14 pm
 class Solution {
-private:
-    long long solve(int i, string &s, vector<int>&dp){
-        if(i== s.size()){
-            return 1;
-        }
+// private:
+//     long long solve(int i, string &s, vector<int>&dp){
+//         if(i== s.size()){
+//             return 1;
+//         }
 
-        if(dp[i]!=-1){
-            return dp[i];
-        }
-        if(s[i]=='0'){
-            return 0;
-        }
-        long long ans= solve(i+1, s, dp);
-        if(i<s.size()-1 ){
-            if(s[i]=='1' || (s[i]=='2' && s[i+1]<'7')){
-                ans+=solve(i+2, s, dp);
-                if(s[i+1]=='0')ans=solve(i+2, s, dp);
-            }
-            else if(s[i+1]=='0'){
-                ans=0;
-            }
-        }
-        return dp[i]=ans;
-    }
+//         if(dp[i]!=-1){
+//             return dp[i];
+//         }
+//         if(s[i]=='0'){
+//             return 0;
+//         }
+//         long long ans= solve(i+1, s, dp);
+//         if(i<s.size()-1 ){
+//             if(s[i]=='1' || (s[i]=='2' && s[i+1]<'7')){
+//                 ans+=solve(i+2, s, dp);
+//             }
+//         }
+//         return dp[i]=ans;
+//     }
 public:
     int numDecodings(string s) {
         int n=s.size();
+        // vector<int>dp(n+1, -1);
+        // return solve(0, s, dp);
         vector<int>dp(n+1, 0);
         if(s[0]=='0'){
             return 0;
         }
         dp[n]=1;
         for(int i=n-1; i>=0; i--){
-            long long ans= dp[i+1];
-            if(i<n-1 ){
-                if(s[i]=='1' || (s[i]=='2' && s[i+1]<'7')){
-                    ans+=dp[i+2];
-                    if(s[i+1]=='0' || (i<n-2 &&s[i+2]=='0'))ans=dp[i+2];
-                }
-                else if(s[i+1]=='0'){
-                    return 0;
+            if(s[i]=='0'){
+                dp[i]=0;
+            }
+            else {
+                dp[i]=dp[i+1];
+                if(i<n-1 && (s[i]=='1' || (s[i]=='2' && s[i+1]<'7'))){
+                    dp[i]+=dp[i+2];
                 }
             }
-            dp[i]=ans;
         }
-        // for(int i=0;i<n;i++){
-        //     cout<<"i= "<<dp[i]<<endl;
-        // }
         return dp[0];
     }
 };
