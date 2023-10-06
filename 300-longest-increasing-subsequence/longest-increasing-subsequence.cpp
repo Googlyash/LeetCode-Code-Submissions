de2475ead46715e5c0bbbf533b993d764f6593a7
Googlyash <77059584+Googlyash@users.noreply.github.com>
2023-10-06 15:58:46 +0530

    Time: 6 ms (90.14%) | Memory: 10.9 MB (38.98%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,20 +1,17 @@
-//Striver SDE Sheet
-//Aug'4, 2023 05:28 pm
-//Recursion with memoization
+//Oct'6, 2023 03:58 pm
 
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
-        int n=nums.size(), ans=1;
-        vector<int> dp(n+1, 0);
-        for(int i=0;i<n;i++)dp[i]=1;
-        for(int i=1;i<n ;i++){
-            for(int j=0; j<=i; j++){
-                if( nums[i]> nums[j])
-                dp[i]=max(dp[i], dp[j]+1);
-                ans=max(ans, dp[i]);
+        int n=nums.size();
+        vector<int>v;
+        for(int i=0;i<n;i++){
+            auto it=lower_bound(v.begin(), v.end(), nums[i])-v.begin();
+            if(it==v.size()){
+                v.push_back(nums[i]);
             }
+            else v[it]=nums[i];
         }
-        return ans;
+        return v.size();
     }
 };
