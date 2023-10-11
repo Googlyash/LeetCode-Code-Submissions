bda1c7b3a0258b3e23dd9e684194283cddaad47e
Googlyash <77059584+Googlyash@users.noreply.github.com>
2023-10-11 15:52:52 +0530

    Time: 282 ms (33.03%) | Memory: 89.2 MB (43.56%) - LeetSync

--- 2334-number-of-flowers-in-full-bloom/number-of-flowers-in-full-bloom.cpp
+++ 2334-number-of-flowers-in-full-bloom/number-of-flowers-in-full-bloom.cpp
@@ -1,42 +1,25 @@
 //POTD Oct'11, 2023
 //Oct'11, 2023 03:04 pm
-//Using Difference Array
-
+//Earlier Solvwed Using Difference Array
+//Comments: Binary search
 class Solution {
-private:
-    void update(vector<int>&d, int l, int r, int x){
-        if(l>r){
-            return;
-        }
-        d[l]+=x;
-        d[r+1]-=x;
-    }
 public:
     vector<int> fullBloomFlowers(vector<vector<int>>& flowers, vector<int>& people) {
-        int n=flowers.size(), m=people.size(), mod=1e9+7;
-        int mx=0;
-        
-        vector<int> d(m+1, 0),p=people;
-        map<int, int>mp;
+        int n=flowers.size(), m=people.size();
 
-        sort(people.begin(), people.end());
+        vector<int>strt, end;
         for(auto it:flowers){
-            int l=lower_bound(people.begin(), people.end(), it[0])-people.begin();
-            int r=upper_bound(people.begin(), people.end(), it[1])-people.begin();
-            cout<<l<<" "<<r<<endl;
-            update(d, l, r-1, 1);
+            strt.push_back(it[0]);
+            end.push_back(it[1]);
         }
-        d.pop_back();
-        // print(d, d);
-        for(int i=1;i<d.size(); i++){
-            d[i]+=d[i-1];
-            mp[people[i]]=d[i];
-        }
-        mp[people[0]]=d[0];
+        sort(strt.begin(), strt.end());
+        sort(end.begin(), end.end());
+
         vector<int>ans;
-        for(int i=0;i<m;i++){
-            // cout<<p[i].second;
-            ans.push_back(mp[p[i]]);
+        for(auto it:people){
+            int r=upper_bound(strt.begin(), strt.end(), it)-strt.begin();
+            int l=lower_bound(end.begin(), end.end(), it)-end.begin();
+            ans.push_back(r-l);
         }
         return ans;
     }
