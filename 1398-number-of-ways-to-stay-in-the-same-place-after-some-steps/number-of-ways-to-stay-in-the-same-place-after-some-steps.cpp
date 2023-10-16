5cdcc434a2710bb5bcb03b191e3ae8326b356acc
Googlyash <77059584+Googlyash@users.noreply.github.com>
2023-10-16 17:38:41 +0530

    Time: 566 ms (5.21%) | Memory: 57.8 MB (6.51%) - LeetSync

--- 1398-number-of-ways-to-stay-in-the-same-place-after-some-steps/number-of-ways-to-stay-in-the-same-place-after-some-steps.cpp
+++ 1398-number-of-ways-to-stay-in-the-same-place-after-some-steps/number-of-ways-to-stay-in-the-same-place-after-some-steps.cpp
@@ -1,29 +1,32 @@
 //POTD Oct'15, 2023
-//Oct'15, 2023 03:10 pm
-
-static int MOD=1e9+7;
+//Oct'16, 2023 05:38 pm
 class Solution {
-public:
-    vector<vector<int>> memo;
-    int arrLen;
-    int dp(int i, int steps)
-    {
-        if(steps==0&&i==0)                                             //Base condition
-            return 1;
-        if(i<0||i>=arrLen||steps==0||i>steps)					//Pruning.
+int mod=1e9+7;
+private:
+    int solve(int i, int cur, int arrLen, int steps, map<pair<int, int>, int>&dp){
+        if(cur==steps){
+            return i==0;
+        }
+        if(i<0 || i>=arrLen){
             return 0;
-        if(memo[i][steps]!=-1)         //If we have already cached the result for current `steps` and `index` get it.
-            return memo[i][steps];
-        return memo[i][steps]=((dp(i+1,steps-1)%MOD+dp(i-1,steps-1))%MOD+dp(i,steps-1))%MOD;        //Either move right, left or stay.
-            
+        }
+        if(dp.find({cur, i})!=dp.end()){
+            return dp[{cur, i}];
+        }
+        int l=0, r=0, stay=0;
+        l=(solve(i-1, cur+1, arrLen, steps, dp))%mod;
+        r=(solve(i+1, cur+1, arrLen, steps, dp))%mod;
+        stay=(solve(i, cur+1, arrLen, steps, dp))%mod;
+
+        return dp[{cur, i}]=((l+r)%mod+stay)%mod;
     }
-    int numWays(int steps, int arrLen) 
-    {
-        memo.resize(steps/2+1,vector<int>(steps+1,-1));
-        this->arrLen=arrLen;
-        return dp(0,steps);
+public:
+    int numWays(int steps, int arrLen) {
+        map<pair<int, int>, int>dp;
+        int ans=solve(0, 0, arrLen, steps, dp);
+        return ans;
     }
 };
 
-
 //Comments
+//40 min
