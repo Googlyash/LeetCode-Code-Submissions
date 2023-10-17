ef267c4d5ccf20523c0ed7e23759d2d3d436d946
Googlyash <77059584+Googlyash@users.noreply.github.com>
2023-10-17 16:42:09 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (63.57%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -5,14 +5,13 @@ public:
     int rob(vector<int>& nums) {
         int n=nums.size();
         if(n==1)return nums[0];
-        vector<int>dp(n+1, 0);
-        dp[0]=nums[0], dp[1]=nums[1];
-        for(int i=2;i<n;i++){
-            for(int j=i-2;j>=0;j--){
-                dp[i]=max(dp[i], dp[j]+nums[i]);
-            }
+        int pre=0, pre2=0;
+        for(int i=0;i<n;i++){
+            int cur=max(pre, pre2+nums[i]);
+            pre2=pre;
+            pre=cur;
         }
-        return max(dp[n-1], dp[n-2]);
+        return max(pre, pre2);
     }
 };
 
