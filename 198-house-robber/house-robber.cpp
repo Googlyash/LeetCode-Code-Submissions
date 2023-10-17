3398d1e9c0066094fa65d39dd19659d77f1073b1
Googlyash <77059584+Googlyash@users.noreply.github.com>
2023-10-17 16:39:11 +0530

    Time: 4 ms (22.71%) | Memory: 8.3 MB (6.20%) - LeetSync

---
+++ 198-house-robber/house-robber.cpp
@@ -0,0 +1,19 @@
+//Oct'17, 2023 04:37 pm
+
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n=nums.size();
+        if(n==1)return nums[0];
+        vector<int>dp(n+1, 0);
+        dp[0]=nums[0], dp[1]=nums[1];
+        for(int i=2;i<n;i++){
+            for(int j=i-2;j>=0;j--){
+                dp[i]=max(dp[i], dp[j]+nums[i]);
+            }
+        }
+        return max(dp[n-1], dp[n-2]);
+    }
+};
+
+//5:30 min
