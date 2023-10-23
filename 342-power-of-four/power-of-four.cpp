f562fd1bfd2ffcbdcb38105310496ddc799aeca9
Googlyash <77059584+Googlyash@users.noreply.github.com>
2023-10-23 16:45:39 +0530

    Time: 0 ms (100.00%) | Memory: 6.5 MB (13.31%) - LeetSync

--- 342-power-of-four/power-of-four.cpp
+++ 342-power-of-four/power-of-four.cpp
@@ -4,36 +4,11 @@
 class Solution {
 public:
     bool isPowerOfFour(int n) {
-        // string s;
         if(n==1)return true;
         if(n<1 || n%4)return false;
-        return isPowerOfFour(n/4);
-        // while(n){
-        //     s+=n%2+'0';
-        //     n/=2;
-        // }
-        // reverse(s.begin(), s.end());
-        // cout<<s;
-        // for(int i=0;i<s.size();i++){
-        //     if(i%2==0){
-        //         continue;
-        //     }
-        //     if(s[i]=='1'){
-        //         return false;
-        //     }
-        // }
-
-        // // if(n==0)return false;
-        // // if(n==1)return true;
-        // // while(abs(n)>3){
-        // //     int k=n%4;
-        // //     cout<<k;
-        // //     if(k>0){
-        // //         return false;
-        // //     }
-        // //     n/=4;
-        // // }
-        return true;
+        double k=log(n)/ log(4);
+        int t=k;
+        return k==(double)t;
     }
 };
 
