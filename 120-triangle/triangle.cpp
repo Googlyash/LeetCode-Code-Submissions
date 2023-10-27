141cf47f9215920ab82cfc87cf52b591097c04f1
Googlyash <77059584+Googlyash@users.noreply.github.com>
2023-10-27 18:57:54 +0530

    Time: 4 ms (75.90%) | Memory: 9 MB (60.30%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -4,17 +4,17 @@ class Solution {
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
         int n=triangle.size();
-        vector<vector<int>>dp(n+1, vector<int>(n+1, 0));
+        vector<int>cur(n+1, 0), pre(n+1, 0);
         for(int i=n-1;i>=0;i--){
-            dp[n-1][i]=triangle[n-1][i];
-            // cout<<dp[n-1][i]<<" ";
+            pre[i]=triangle[n-1][i];
         }
         for(int i=n-2;i>=0;i--){
             for(int j=0;j<=i;j++){
-                dp[i][j]=min(dp[i+1][j], dp[i+1][j+1])+triangle[i][j];
+                cur[j]=min(pre[j], pre[j+1])+triangle[i][j];
             }
+            pre=cur;
         }
-        return dp[0][0];
+        return pre[0];
     }
 };
 
