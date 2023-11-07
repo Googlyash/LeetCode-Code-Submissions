18055a2e3035957e12c72e98ec5b76e068e07570
Googlyash <77059584+Googlyash@users.noreply.github.com>
2023-11-07 16:47:00 +0530

    Time: 119 ms (79.50%) | Memory: 44.3 MB (5.02%) - LeetSync

---
+++ 2089-maximum-matrix-sum/maximum-matrix-sum.cpp
@@ -0,0 +1,25 @@
+//POTD Nov'7, 2023
+//Nov'7, 2023 04:46 pm
+
+#define ll long long
+class Solution {
+public:
+    long long maxMatrixSum(vector<vector<int>>& matrix) {
+        int n=matrix.size(), m=matrix[0].size(), z=1e6, o=0;
+        vector<vector<ll>>v(n, vector<ll>(m, 0));
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                z=min(abs(matrix[i][j]), z);
+                o+= (matrix[i][j]<0);
+            }
+        }
+        ll cur=0; 
+        for(int i=0;i<n;i++){
+            for(auto it:matrix[i]){
+                cur+= abs(it);
+            }
+        }
+        if(o%2)cur-= 2*z;
+        return cur;
+    }
+};
