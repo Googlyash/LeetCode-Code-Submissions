1cbfd86f301fdcf604ef74986724734972d75f38
Googlyash <77059584+Googlyash@users.noreply.github.com>
2024-03-13 21:55:51 +0530

    Time: 0 ms (100.00%) | Memory: 7.3 MB (41.86%) - LeetSync

---
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.cpp
@@ -0,0 +1,17 @@
+//POTD Mar'13, 2024
+//Mar'13, 2024 09:55 pm
+
+class Solution {
+public:
+    int pivotInteger(int n) {
+        int ans=0;
+        for(int i=1;i<=n;i++){
+            if((i*i)== (n*(n+1))/2){
+                return i;
+            }
+        }
+        return -1;
+    }
+};
+
+//4 min
