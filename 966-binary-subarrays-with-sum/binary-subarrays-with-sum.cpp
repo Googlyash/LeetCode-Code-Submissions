968c461c9470a588023f7be3f6f025cbbc5de945
Googlyash <77059584+Googlyash@users.noreply.github.com>
2024-03-14 23:40:53 +0530

    Time: 44 ms (40.99%) | Memory: 40.1 MB (10.17%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
@@ -1,11 +1,14 @@
+//POTD Mar'14, 2024
+//Mar'14, 2024 11:40 pm
+
 class Solution {
 public:
-        int numSubarraysWithSum(vector<int>& A, int S) {
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
         unordered_map<int, int> c({{0, 1}});
         int psum = 0, res = 0;
-        for (int i : A) {
+        for (int i : nums) {
             psum += i;
-            res += c[psum - S];
+            res += c[psum - goal];
             c[psum]++;
         }
         return res;
