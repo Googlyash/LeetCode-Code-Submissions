24bd451a23b9cad021b780a27119775df72c3893
Googlyash <77059584+Googlyash@users.noreply.github.com>
2024-04-08 23:59:53 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (95.64%) - LeetSync

---
+++ 1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.cpp
@@ -0,0 +1,14 @@
+//POTD Apr'8, 2024
+//Apr'8, 2024 11:59 pm
+
+class Solution {
+public:
+    int countStudents(vector<int>& A, vector<int>& B) {
+        int count[] = {0, 0}, n = A.size(), k;
+        for (int a : A)
+            count[a]++;
+        for (k = 0; k < n && count[B[k]] > 0; ++k)
+            count[B[k]]--;
+        return n - k;
+    }
+};
