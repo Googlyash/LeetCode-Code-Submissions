db901b366ce4f7366edf520193728c27a41647ea
Googlyash <77059584+Googlyash@users.noreply.github.com>
2024-05-06 20:54:21 +0530

    Time: 247 ms (93.06%) | Memory: 164.4 MB (65.64%) - LeetSync

---
+++ 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.cpp
@@ -0,0 +1,23 @@
+//POTD May'6, 2024
+//May'6, 2024 08:54 pm
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeNodes(ListNode* head) {
+        if (!head) return NULL;
+        head->next = removeNodes(head->next);
+        return head->next && head->val < head->next->val ?  head->next : head;
+    }
+};
+
+//Comments
