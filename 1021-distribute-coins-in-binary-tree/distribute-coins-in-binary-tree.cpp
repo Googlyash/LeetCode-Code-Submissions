2a4a85d98fa0d1d0a2453070e3593093a5c2f0ff
Googlyash <77059584+Googlyash@users.noreply.github.com>
2024-05-18 09:14:56 +0530

    Time: 3 ms (80.19%) | Memory: 15.6 MB (58.63%) - LeetSync

---
+++ 1021-distribute-coins-in-binary-tree/distribute-coins-in-binary-tree.cpp
@@ -0,0 +1,25 @@
+//POTD May'18, 2024
+//May'18, 2024 09:14 am
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int distributeCoins(TreeNode* r, TreeNode* p = nullptr) {
+        if (r == nullptr) return 0;
+        int res = distributeCoins(r->left, r) + distributeCoins(r->right, r);
+        if (p != nullptr) p->val += r->val - 1;
+        return res + abs(r->val - 1);
+    }
+};
+
+//Comments
