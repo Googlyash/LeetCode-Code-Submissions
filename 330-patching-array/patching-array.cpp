2b849ab7157836e74bb32a2c85013fe9be56b24c
Googlyash <77059584+Googlyash@users.noreply.github.com>
2024-06-16 21:07:27 +0530

    Time: 3 ms (83.44%) | Memory: 13.7 MB (82.46%) - LeetSync

---
+++ 330-patching-array/patching-array.cpp
@@ -0,0 +1,18 @@
+//POTD June'16, 2024
+//June'16, 2024 09:07 pm
+
+class Solution {
+public:
+    int minPatches(vector<int>& nums, int n) {
+        long miss = 1, added = 0, i = 0;
+        while (miss <= n) {
+            if (i < nums.size() && nums[i] <= miss) {
+                miss += nums[i++];
+            } else {
+                miss += miss;
+                added++;
+            }
+        }
+        return added;
+    }
+};
