e8a77f90da64c4163544d7d91b2bd0b90e0ffc8d
Googlyash <77059584+Googlyash@users.noreply.github.com>
2024-07-10 00:03:55 +0530

    Time: 139 ms (85.88%) | Memory: 76.4 MB (83.82%) - LeetSync

--- 1803-average-waiting-time/average-waiting-time.cpp
+++ 1803-average-waiting-time/average-waiting-time.cpp
@@ -3,21 +3,16 @@
 
 class Solution {
 public:
-    double averageWaitingTime(vector<vector<int>>& customers) {
-        int n = customers.size();
-        double time_waiting = customers[0][1];
-        int finished_prev = customers[0][0] + customers[0][1];
+    double averageWaitingTime(vector<vector<int>>& c) {
+        long long n=c.size(), tot=0, cur=c[0][0] ;
+        double ans=0;
 
-        for (int customer_ind = 1; customer_ind < n; ++customer_ind) {
-            vector<int> times = customers[customer_ind];
-            int arrive = times[0];
-
-            int start_cook = max(arrive, finished_prev);
-            int end_time = start_cook + times[1];
-            finished_prev = end_time;
-            time_waiting += end_time - arrive;
+        for(int i=0;i<n;i++){
+            cur=c[i][1]+ max(cur, (long long)c[i][0]);
+            tot+= cur-c[i][0];
         }
 
-        return time_waiting / n;
+        ans= (double)tot/(double)n;
+        return ans;
     }
 };
