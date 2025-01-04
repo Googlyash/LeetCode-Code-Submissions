b8cf921c915018348b3a34eca9bc9778a7a46b0f
Googlyash <77059584+Googlyash@users.noreply.github.com>
2025-01-04 14:51:26 +0530

    Time: 24 ms (50.16%) | Memory: 70.6 MB (36.15%) - LeetSync

--- 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,27 +1,37 @@
-//POTD June'19, 2024
-//June'19, 2024 11:02 pm
+// Jan'4, 2024 02:51 pm
+
 
 class Solution {
 public:
-    int minDays(vector<int>& A, int m, int k) {
-        int n = A.size(), left = 1, right = 1e9;
-        if ((1LL)*m *(1LL)* k > n) return -1;
-        while (left < right) {
-            int mid = (left + right) / 2, flow = 0, bouq = 0;
-            for (int j = 0; j < n; ++j) {
-                if (A[j] > mid) {
-                    flow = 0;
-                } else if (++flow >= k) {
+    int minDays(vector<int>& bloomDay, int m, int k) {
+        int n= bloomDay.size();
+
+        if((1LL)*m*(1LL)*k> n){
+            return -1;
+        }
+        int left=1, right= 1e9+7;
+        while(left<= right){
+            int mid= left+ (right - left)/2;
+            int adj=0, bouq=0;
+
+            for(int i=0;i<n;i++){
+                if(bloomDay[i]> mid){
+                    adj=0;
+                }
+                else if(++adj >=k){
+                    adj=0;
                     bouq++;
-                    flow = 0;
                 }
             }
-            if (bouq < m) {
-                left = mid + 1;
-            } else {
-                right = mid;
+
+            if(bouq>=m){
+                right= mid-1;
+            }
+            else {
+                left= mid+1;
             }
         }
+
         return left;
     }
 };
