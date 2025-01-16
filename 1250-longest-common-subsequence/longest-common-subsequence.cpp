b80d3edc50343d78ad0a4edf1c9395d7c925fe65
Googlyash <77059584+Googlyash@users.noreply.github.com>
2025-01-16 15:43:43 +0530

    Time: 33 ms (31.87%) | Memory: 27.3 MB (57.33%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,25 +1,43 @@
-//Striver SDE Sheet
-//Aug'5, 2023 05:37 pm
-//Tabulation with space optimization
+// Jan'16, 2025 03:43 pm
 
 class Solution {
 public:
-    int longestCommonSubsequence(string s1, string s2) {
-        int n=s1.size(), m=s2.size(), ans=0;
-        // vector<vector<int>>dp(n+1, vector<int>(m+1, 0));
+    int longestCommonSubsequence(string text1, string text2) {
+        int n =text1.size(), m= text2.size();
 
-        vector<int>pre(m+1, 0), cur(m+1, 0);
-        for(int i=1;i<=n;i ++){
+        // return solve(n, m, text1, text2);
+
+        vector<vector<int>>dp(n+1, vector<int>(m+1, 0));
+
+        for(int i=1;i<=n;i++){
             for(int j=1;j<=m;j++){
-                if(s1[i-1]==s2[j-1]){
-                    cur[j]= 1+pre[j-1];
+                int nottake= max( dp[i-1][j-1], max( dp[i][j-1], dp[i-1][j]));
+                int take=0;
+                if(text1[i-1]== text2[j-1]){
+                    take= 1 + dp[i-1][j-1];
                 }
-                else 
-                cur[j]= max(pre[j], cur[j-1]);
+
+                dp[i][j]= max(take, nottake);
             }
-            pre=cur;
         }
 
-        return pre[m];
+        return dp[n][m];
     }
 };
+
+// 9 min
+
+// Recursion
+    // int solve(int i, int j, string t1, string t2){
+    //     if(i==0 || j==0){
+    //         return 0;
+    //     }
+
+    //     int nottake= max(solve(i-1, j-1, t1, t2), max(solve(i, j-1, t1, t2), solve(i-1, j, t1, t2)));
+    //     int take=0;
+    //     if(t1[i-1]== t2[j-1]){
+    //         take= 1 + solve(i-1, j-1, t1, t2);
+    //     }
+
+    //     return max(take, nottake);
+    // }
