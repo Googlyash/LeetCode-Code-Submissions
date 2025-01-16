747b066a206ba9e698e42b887a04aefb815a0471
Googlyash <77059584+Googlyash@users.noreply.github.com>
2025-01-16 17:07:04 +0530

    Time: 6 ms (80.41%) | Memory: 22.7 MB (84.61%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -1,32 +1,33 @@
-//Sep'16, 2023 03:33 pm
-//Self: 6Min
+// Jan'16, 2025 05:06 pm
 
-#define ll long long
 class Solution {
 public:
     int minEatingSpeed(vector<int>& piles, int h) {
-        ll sum=0;
-        for(auto it:piles){
-            sum+=it;
-        }
-        ll l=1, r=1e9;
-        while(l<=r){
-            ll mid= l+(r-l)/2;
-            ll k=0;
+        int n= piles.size();
+
+        int l= 1, r= 1e9;
+
+        while(l<r){
+            int mid= l+ (r-l)/2;
+
+            int cnt=0;
             for(auto it:piles){
-                k+=it/mid;
-                if(it%mid>0)k++;
+                cnt+= it/mid;
+                if(it%mid!=0){
+                    cnt++;
+                }
             }
-            if(k<=h){
-                r=mid-1;
+
+            if(cnt<=h){
+                r= mid;
             }
             else {
                 l=mid+1;
             }
         }
+
         return l;
     }
 };
 
-//[332484035,524908576,855865114,632922376,222257295,690155293,112677673,679580077,337406589,290818316,877337160,901728858,679284947,688210097,692137887,718203285,629455728,941802184]
-//h= 823855818
+// 8 min
