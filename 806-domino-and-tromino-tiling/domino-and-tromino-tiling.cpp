53f20215402c8559b4346ff96f323562161a0227
Googlyash <77059584+Googlyash@users.noreply.github.com>
2025-01-23 18:45:49 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (15.81%) - LeetSync

---
+++ 806-domino-and-tromino-tiling/domino-and-tromino-tiling.cpp
@@ -0,0 +1,43 @@
+// Jan'23, 2025 06:45 pm
+
+class Solution {
+    int solve(int i, int n, bool prevGap){
+        int mod= 1e9+7;
+        if(i>n){
+            return 0;
+        }
+        if(i==n){
+            return !prevGap;
+        }
+
+        if(prevGap){
+            return (solve(i+1, n, false)+ solve(i+1, n, true))%mod;
+        }
+        else {
+            return (solve(i+1, n, false)+ solve(i+2, n, false)+ 2*solve(i+2, n, true))%mod;
+        }
+    }
+public:
+    int numTilings(int n) {
+        // vector<vector<int>>dp 
+        // return solve(0, n, false);
+
+        int mod= 1e9+7;
+        vector<long long>dpt(n+1, 0), dpf(n+1, 0);
+        dpf[n]=1;
+
+        for(int i=n-1; i>=0; i--){
+            dpt[i]= (dpf[i+1]+ dpt[i+1])%mod;
+            dpf[i]= (dpf[i+1] )%mod;
+            if(i<n-1){
+                dpf[i]+= dpf[i+2]+ 2ll*dpt[i+2];
+                dpf[i]%=mod;
+            }
+        }
+
+        return dpf[0];
+    }
+};
+
+// Comments
+// 01 hour 08 min
