62d5c0fe50f8501ef9fc41cc69da8d4edd5480e7
Googlyash <77059584+Googlyash@users.noreply.github.com>
2025-01-23 19:03:26 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (86.19%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,18 +1,36 @@
-//Oct'17, 2023 04:37 pm
+// Jan'23, 2025 07:02 pm
 
 class Solution {
+    int solve(int i, int lastPick, vector<int>&nums){
+        if(i==0){
+            return lastPick==0 ? nums[0]: 0;
+        }
+
+        int notPick= solve(i-1, 0, nums);
+        int pick=0;
+        if(!lastPick){
+            pick= nums[i]+ solve(i-1, 1, nums);
+        }
+
+        return max(pick, notPick);
+    }
 public:
     int rob(vector<int>& nums) {
-        int n=nums.size();
-        if(n==1)return nums[0];
-        int pre=0, pre2=0;
-        for(int i=0;i<n;i++){
-            int cur=max(pre, pre2+nums[i]);
-            pre2=pre;
-            pre=cur;
+        int n= nums.size();
+
+        // return solve(n-1, 0, nums);
+
+        vector<int>dp(2, 0);
+        dp[0]=nums[0];
+        for(int i=1;i<n;i++){
+            int notPick= dp[0];
+            int pick= nums[i]+ dp[1];
+
+            dp[0]= max(pick, notPick);
+            dp[1]= notPick;
         }
-        return max(pre, pre2);
+        return max(dp[0], dp[1]);
     }
 };
 
-//5:30 min
+// 14 min
