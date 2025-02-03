411d35cbc4d1be883d274dea0c5c8925542e3a29
Googlyash <77059584+Googlyash@users.noreply.github.com>
2025-02-03 21:38:43 +0530

    Time: 5 ms (51.58%) | Memory: 105.7 MB (16.07%) - LeetSync

--- 463-island-perimeter/island-perimeter.cpp
+++ 463-island-perimeter/island-perimeter.cpp
@@ -1,31 +1,42 @@
-//POTD Apr'18, 2024
-//Apr'18, 2024 08:35 pm
+// Feb'3, 2025 09:38 pm
 
 class Solution {
-public:
-    int islandPerimeter(vector<vector<int>>& grid) {
-        int n=grid.size(), m=grid[0].size();
+    int dfs(int i, int j, vector<vector<int>>&grid, vector<vector<int>>&vis){
+        int n= grid.size(), m= grid[0].size();
+        vis[i][j]=1;
 
-        int del[]= {0, -1, 0, 1, 0};
+        int dir[]= {0, -1, 0, 1, 0};
         int ans=0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(grid[i][j]==1){
-                    for(int k=0;k<4;k++){
-                        int r= i+ del[k];
-                        int c= j+ del[k+1];
-                        if(min(r,c)>=0 && r<n && c<m && grid[r][c]!=1){
-                            ans++;
-                        }
-                        else if(min(r,c)<0 || r>=n || c>=m){
-                            ans++;
-                        }
-                    }
-                }
+        for(int k=0; k<4;k++){
+            int r= i+ dir[k];
+            int c= j+ dir[k+1];
+
+            if(min(r,c)>=0 && r<n && c<m && grid[r][c]==1){
+                if(!vis[r][c]) ans+=dfs(r, c, grid, vis);
+            }
+            else {
+                ans++;
             }
         }
         return ans;
     }
+public:
+    int islandPerimeter(vector<vector<int>>& grid) {
+        int n= grid.size(), m=grid[0].size();
+        int ans=0;
+
+        vector<vector<int>>vis(n, vector<int>(m, 0));
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(!vis[i][j] && grid[i][j]==1){
+                    ans+=dfs(i,j, grid, vis);
+                }
+            }
+        }
+
+        return ans;
+    }
 };
 
-//10 min
+// 8 min
