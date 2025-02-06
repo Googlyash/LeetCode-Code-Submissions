0785716a316c681694356a623145e8d8a62dc284
Googlyash <77059584+Googlyash@users.noreply.github.com>
2025-02-06 17:33:50 +0530

    Time: 931 ms (6.27%) | Memory: 99.3 MB (10.34%) - LeetSync

--- 1364-tuple-with-same-product/tuple-with-same-product.cpp
+++ 1364-tuple-with-same-product/tuple-with-same-product.cpp
@@ -6,30 +6,18 @@ public:
     int tupleSameProduct(vector<int>& nums) {
         int n= nums.size();
 
-        map<long long, vector<int>>mp;
+        int ans=0;
+        map<int, int>mp;
         for(int i=0;i<n;i++){
             for(int j=i+1; j<n;j++){
-                long long prod= nums[i]* nums[j];
-                if(nums[i]!=nums[j]){
-                    mp[prod].push_back(nums[i]);
-                    mp[prod].push_back(nums[j]);
-                }
+                int prod= nums[i]* nums[j];
+                ans+= 8* mp[prod];
+                mp[prod]++;
             }
         }
-
-        int ans=0;
-        for(auto it:mp){
-            vector<int>v= it.second;
-
-            int sz= v.size();
-            // cout<<it.first<<" "<<sz<<endl;
-            long long cur= (sz/2)*(sz/2-1)*4;
-            // cur*=8;
-            ans+=cur;
-        }
-
         return ans;
     }
 };
 
 // 20 min
+// Earlier solved
