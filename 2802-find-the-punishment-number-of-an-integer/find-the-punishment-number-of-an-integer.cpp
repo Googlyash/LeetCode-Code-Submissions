5cecd94e99d0ef2bf80bb7434e181cb30c734ce9
Googlyash <77059584+Googlyash@users.noreply.github.com>
2025-02-15 23:42:22 +0530

    Time: 2 ms (89.67%) | Memory: 7.9 MB (91.69%) - LeetSync

---
+++ 2802-find-the-punishment-number-of-an-integer/find-the-punishment-number-of-an-integer.cpp
@@ -0,0 +1,28 @@
+// POTD Feb'15, 2025
+// Feb'15, 2025 11:41 pm
+
+class Solution {
+public:
+    bool partition(int x, int target) {
+        if (x==target) return 1;
+        if (x==0) return target==0; 
+        const int m0=min(x, 1000); 
+        [[unroll]] 
+        for (int m=10; m<=m0; m*=10) {
+            if (partition(x/m, target-x%m)) return 1;
+        }
+        return 0;
+    }
+
+    int punishmentNumber(int n) {
+        int sum=0;
+        for (int i=1, step[2]={8, 1}, j=0; i<=n; i+=step[j], j=1-j) {
+            const int x=i*i;
+            sum+=(partition(x, i))?x:0;
+        }
+        return sum;
+    }
+};
+
+// Comments
+// 
