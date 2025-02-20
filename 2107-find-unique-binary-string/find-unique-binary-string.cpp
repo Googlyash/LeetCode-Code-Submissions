e367ff4d0f8b2055c26e51b4a3a94ba53dd3fc40
Googlyash <77059584+Googlyash@users.noreply.github.com>
2025-02-20 23:41:34 +0530

    Time: 1419 ms (8.36%) | Memory: 251 MB (7.18%) - LeetSync

--- 2107-find-unique-binary-string/find-unique-binary-string.cpp
+++ 2107-find-unique-binary-string/find-unique-binary-string.cpp
@@ -1,39 +1,41 @@
+// POTD Feb'20, 2025
+// Feb'20, 2025 11:41 pm
+
 class Solution {
-public:
-    int tr(string y){
-        reverse(y.begin(),y.end());
-        int yr=0;
-        for(int i=0;i<y.size();i++){
-            yr+=(pow(2,i)*(y[i]-'0'));
+    void solve(int n, string &ans, string s, int flag, map<string, int>&mp){
+        if(flag){
+            return ;
         }
-        return yr;
-    }
-    string bs(int u){
-        string y;
-        while(u){
-            int yr=u%2;
-            u/=2;
-            y+=(yr+'0');
-        }
-        reverse(y.begin(),y.end());
-        return y;
-    }
-    string findDifferentBinaryString(vector<string>& nums) {
-        int n=nums.size();
-        map<int,int> m;
-        for(int i=0;i<nums.size();i++){
-            m[tr(nums[i])]++;
-        }
-        for(int i=0;i<pow(2,n);i++){
-            if(m[i]==0){
-                string yr=bs(i);
-                reverse(yr.begin(),yr.end());
-                while(yr.size()!=n) yr+='0';
-                reverse(yr.begin(),yr.end());
-                return yr;
+        if(s.size()== n){
+            if(mp.find(s)==mp.end()){
+                ans= s;
+                flag=1;
             }
+            return;
         }
-        string ut;
-        return ut;
+
+        s+='0';
+        solve(n, ans, s, flag, mp);
+        s.pop_back();
+
+        s+='1';
+        solve(n, ans, s, flag, mp);
+        s.pop_back();
+    }
+public:
+    string findDifferentBinaryString(vector<string>& nums) {
+        int n= nums.size();
+        map<string, int>mp;
+        for(auto it:nums){
+            mp[it]++;
+        }
+
+        int flag=0;
+        string ans;
+        vector<string>v;
+        solve(nums.size(), ans, "", flag, mp);
+        return ans;
     }
 };
+
+// 9 min
