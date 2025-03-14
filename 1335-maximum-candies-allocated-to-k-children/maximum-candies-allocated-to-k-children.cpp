9ad8bc7ca5df4cf4feaa042bdbecf03ca98738e9
Googlyash <77059584+Googlyash@users.noreply.github.com>
2025-03-14 22:22:09 +0530

    Time: 26 ms (63.84%) | Memory: 88.1 MB (73.33%) - LeetSync

---
+++ 1335-maximum-candies-allocated-to-k-children/maximum-candies-allocated-to-k-children.cpp
@@ -0,0 +1,22 @@
+// POTD MAr'14, 2025
+// Mar'14, 2025 10:21 pm
+
+class Solution {
+public:
+    int maximumCandies(vector<int>& A, long long k) {
+        int left = 0, right = 1e7;
+        while (left < right) {
+            long sum = 0, mid = (left + right + 1) / 2;
+            for (int& a : A) {
+                sum += a / mid;
+            }
+            if (k > sum)
+                right = mid - 1;
+            else
+                left = mid;
+        }
+        return left;
+    }
+};
+
+// Comments
