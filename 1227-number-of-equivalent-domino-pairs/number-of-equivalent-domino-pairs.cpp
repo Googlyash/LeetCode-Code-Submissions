e015a58bbbafcebd3d51bcb307cab38d6219641d
Googlyash <77059584+Googlyash@users.noreply.github.com>
2025-05-04 21:52:09 +0530

    Time: 3 ms (58.50%) | Memory: 26.3 MB (48.50%) - LeetSync

---
+++ 1227-number-of-equivalent-domino-pairs/number-of-equivalent-domino-pairs.cpp
@@ -0,0 +1,14 @@
+// POTD May'4, 2025
+// May'4, 2025 09:51 pm
+
+class Solution {
+public:
+    int numEquivDominoPairs(vector<vector<int>>& dominoes) {
+        unordered_map<int, int> count;
+        int res = 0;
+        for (auto& d : dominoes) {
+            res += count[min(d[0], d[1]) * 10 + max(d[0], d[1])]++;
+        }
+        return res;
+    }
+};
