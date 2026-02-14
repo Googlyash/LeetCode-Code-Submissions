7b567700c9a88beedc5d9cd83a95f6e3bc8ad263
Googlyash <77059584+Googlyash@users.noreply.github.com>
2026-02-14 21:12:49 +0530

    Time: 8 ms (33.66%) | Memory: 17.5 MB (60.78%) - LeetSync

--- 815-champagne-tower/champagne-tower.cpp
+++ 815-champagne-tower/champagne-tower.cpp
@@ -1,37 +1,21 @@
-//POTD Sep'24, 2023
-//Sep'24, 2023 03:40 pm
-//Comments: 42 min
+//POTD Feb'14, 2026
+// Feb'14, 2026 08:36 pm
 
 class Solution {
-private:
-    double solve(int poured, int row, int glass, vector<vector<double>>&dp){
-        if(row<0 || glass>row || glass<0){
-            return 0.00;
-        }
-        if(row==0 && glass==0){
-            return poured;
-        }
-
-        if(dp[row][glass]>-1){
-            return dp[row][glass];
-        }
-        double left= (solve(poured, row-1, glass-1, dp)-1)/2;
-        if(left<0 ){
-            left=0;
-        }
-
-        double right= (solve(poured, row-1, glass, dp)-1)/2;
-        if(right<0){
-            right=0;
-        }
-        double tot=left+ right;
-        return dp[row][glass]=tot;
-
-
-    }
 public:
     double champagneTower(int poured, int query_row, int query_glass) {
-        vector<vector<double>>dp(query_row+1, vector<double>(query_glass+1, -1));
-        return min(1.00, solve(poured, query_row, query_glass, dp));
+
+        vector<vector<double>>dp(query_row+1, vector<double>(query_row+1, 0));
+        dp[0][0]=poured;
+        for(int i=1; i<=query_row; i++){
+            for(int j=0;j<=query_glass;j++){
+                if(dp[i-1][j]>=1)dp[i][j]=(dp[i-1][j]-1)/2;
+                if(j>0 && dp[i-1][j-1]>=1)dp[i][j]+=(dp[i-1][j-1]-1)/2;
+            }
+        }
+
+        return min(1.00, dp[query_row][query_glass]);
     }
 };
+
+//Comments
