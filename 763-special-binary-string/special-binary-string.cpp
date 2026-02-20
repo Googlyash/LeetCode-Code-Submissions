73eeeb267f52d2556dffae14f64dd4b25df0d129
Googlyash <77059584+Googlyash@users.noreply.github.com>
2026-02-21 02:12:49 +0530

    Time: 2 ms (34.96%) | Memory: 9.9 MB (18.05%) - LeetSync

---
+++ 763-special-binary-string/special-binary-string.cpp
@@ -0,0 +1,33 @@
+// POTD Feb'20, 2026
+// Feb'21, 2026 02:13 am
+
+class Solution {
+public:
+    string makeLargestSpecial(string s) {
+        int i = 0;
+        int one = 0;
+        int zero = 0;
+        int n = s.size();
+        vector<string> res;
+        for (int j = 0; j < n; j++) {
+            if (s[j] == '1') {
+                one++;
+            } else {
+                zero++;
+            }
+            if (one == zero) {
+                res.push_back(
+                    '1' + makeLargestSpecial(s.substr(i + 1, j - i - 1)) + '0');
+                i = j + 1;
+            }
+        }
+        sort(res.rbegin(), res.rend());
+        string ans;
+        for (auto str : res) {
+            ans += str;
+        }
+        return ans;
+    }
+};
+
+// Comments
