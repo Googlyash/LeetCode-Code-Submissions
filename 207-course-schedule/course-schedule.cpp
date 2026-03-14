7cfa8e7ecf7b946b9706684d2ecb4c1f8ee7066d
Googlyash <77059584+Googlyash@users.noreply.github.com>
2026-03-14 21:59:50 +0530

    Time: 7 ms (47.86%) | Memory: 19.7 MB (43.84%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,43 +1,47 @@
-//Graph Revision
-//Sep'6, 2023 06:51 pm
+// Mar'14, 2026 09:59 pm
+// Graph Practice
 
 class Solution {
+private:
+    bool checkCycle(int cur, vector<int>&vis, vector<int>&pathVis, vector<int>adj[]){
+        vis[cur]=1;
+        pathVis[cur]=1;
+
+        for(auto neighbor: adj[cur]){
+            // cout<<cur<<" "<<neighbor<<" "<<vis[neighbor]<<endl;
+            if(!vis[neighbor]){
+                pathVis[neighbor]=1;
+                if(checkCycle(neighbor, vis, pathVis, adj)){
+                    return true;
+                }
+                pathVis[neighbor]=0;
+            }
+            else if(pathVis[neighbor]){
+                return true;
+            }
+        }
+        pathVis[cur]=0;
+        return false;
+    }
 public:
-    bool canFinish(int n, vector<vector<int>>& prerequisites) {
-        vector<int>adj[n];
-        for(auto it:prerequisites){
-            adj[it[1]].push_back(it[0]);
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<int>adj[numCourses];
+
+        for(auto pre: prerequisites){
+            adj[pre[1]].push_back(pre[0]);
         }
 
-        vector<int>indegree(n, 0);
-        for(int i=0;i<n;i++){
-            for(auto it:adj[i]){
-                indegree[it]++;
-            }
-        }
-
-        queue<int>q;
-        for(int i=0;i<n;i++){
-            if(indegree[i]==0){
-                q.push(i);
-            }
-        }
-
-        int cnt=0;
-        while(!q.empty()){
-            int node=q.front();
-            q.pop();
-            cnt++;
-
-            for(auto it:adj[node]){
-                indegree[it]--;
-                if(indegree[it]==0){
-                    q.push(it);
+        vector<int>vis(numCourses, 0);
+        vector<int>pathVis(numCourses, 0);
+        for(int i=0;i<numCourses; i++){
+            if(!vis[i]){
+                if(checkCycle(i, vis, pathVis, adj)){
+                    return false;
                 }
             }
         }
-        return cnt==n;
+        return true;
     }
 };
 
-//Using Kann's ALgo (TopoSort using BFS)
+// 17 min
