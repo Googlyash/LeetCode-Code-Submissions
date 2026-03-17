dad9f7880a3c9c20b05a882fe04bb513d9b9d656
Googlyash <77059584+Googlyash@users.noreply.github.com>
2026-03-17 20:55:54 +0530

    Time: 73 ms (6.39%) | Memory: 37.9 MB (21.54%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -1,25 +1,43 @@
-//Aug'29, 2023 03:29 pm
+// Mar'17, 2026 08:56 pm
+// DP Practice
 
 class Solution {
+    int solve(int i, int amount, vector<int>&v, set<vector<int>>&st, vector<int>&coins, vector<vector<int>>&dp){
+        int n= coins.size();
+        if(amount<=0 || i==n){
+            if(amount==0){
+                // for(auto it:v)cout<<it<<" ";
+                // cout<<endl;
+                return 1;
+            }
+            return 0;
+        }
+
+        if(dp[i][amount]!=-1){
+            return dp[i][amount];
+        }
+
+        int ans=0;
+        // Not take
+        ans+= solve(i+1, amount, v, st, coins, dp);
+        // take
+        if(amount>= coins[i]){
+            v.push_back(coins[i]);
+            ans+= max(solve(i, amount-coins[i], v, st, coins, dp), solve(i+1, amount-coins[i], v, st, coins, dp));
+            v.pop_back();
+        }
+        return dp[i][amount]= ans;
+    }
 public:
     int change(int amount, vector<int>& coins) {
-        int n=coins.size();
+        int n= coins.size();
 
-        vector<vector<int>>dp(n+1, vector<int>(amount+1, 0));
-        for(int i=0;i<=n;i++){
-            dp[i][0]=1;
-        }
-
-        for(int i=1;i<=n;i++){
-            for(int cnt=1; cnt<=amount; cnt++){
-                int nottake= dp[i-1][cnt];
-                int take=0;
-                if(coins[i-1]<=cnt){
-                    take= dp[i][cnt-coins[i-1]];
-                }
-                dp[i][cnt]= take+nottake;
-            }
-        }
-        return dp[n][amount];
+        vector<int>v;
+        set<vector<int>>st;
+        vector<vector<int>>dp(n+1, vector<int>(amount+1, -1));
+        return solve(0, amount, v, st, coins, dp);
+        // return st.size();
     }
 };
+
+// 17 min
