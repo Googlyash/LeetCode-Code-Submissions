d20fa59af3bbd1cad16e8b41044c96fc60cb04aa
Googlyash <77059584+Googlyash@users.noreply.github.com>
2026-03-17 21:22:39 +0530

    Time: 1 ms (76.39%) | Memory: 12.6 MB (64.24%) - LeetSync

--- 139-word-break/word-break.cpp
+++ 139-word-break/word-break.cpp
@@ -1,19 +1,39 @@
-//Striver: SDE Sheet
-//Aug'8, 2023 09:02 pm
+// Mar'17, 2026 09:20 pm
+// DP Practice
 
 class Solution {
-public:
-    bool wordBreak(string s, vector<string>& wordDict) {
-        int n=s.size(), last=n-1;
-        vector<int>dp(n+1, 0);
-        dp[n]=1;
-        for(int i=n-1; i>=0; i--){
-            for(auto word: wordDict){
-                if(i+ word.size()>n || s.substr(i, word.size())!=word)continue;
-                dp[i]=dp[i+word.size()];
-                if(dp[i]==1)break;
+    bool breakHelper(int cur, vector<int>&dp, string s, vector<string>& wordDict){
+        int n= s.size();
+        if(cur == n){
+            return true;
+        }
+
+        if(dp[cur]!=-1){
+            return dp[cur];
+        }
+        for(auto word: wordDict){
+            int curN= word.size();
+            if(n>= cur+ curN){
+                int i=0;
+                for(i=0;i<curN; i++){
+                    if(word[i]!= s[i+cur]){
+                        break;
+                    }
+                }
+                if(i==curN){
+                    if(breakHelper(cur+curN, dp, s, wordDict)){
+                        return dp[cur]= true;
+                    }
+                }
             }
         }
-        return dp[0];
+        return dp[cur]= false;
+    }
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        vector<int>dp(s.size()+1, -1);
+        return breakHelper(0, dp, s, wordDict);
     }
 };
+
+// 8 min
