bad171aca3b40fbc00eed7e95d774d0962acb999
Googlyash <77059584+Googlyash@users.noreply.github.com>
2026-05-06 01:30:25 +0530

    Time: 0 ms (100.00%) | Memory: 16.4 MB (62.96%) - LeetSync

--- 61-rotate-list/rotate-list.cpp
+++ 61-rotate-list/rotate-list.cpp
@@ -1,45 +1,29 @@
-//Sep'13, 2023 11:10 pm
-//Striver SDE: Day7 LinkedList
+// POTD May'5, 2026
+// May'6, 2026 01:30 am
 
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
-class Solution {
+class Solution{
 public:
-    ListNode* rotateRight(ListNode* head, int k) {
-        if(!head)return head;
-        ListNode*temp=head;
-        ListNode*updated=new ListNode(0);
-        ListNode*upTemp=updated;
-        int n=0, i=0;
-        while(temp){
-            temp=temp->next;
-            n++;
-        }
-        temp=head;
-        k%=n;
-        if(k==0)return head;
-        while(i<n-k){
-            i++;
-            upTemp->next=temp;
-            temp=temp->next;
-            upTemp=upTemp->next;
-        }
-        head=temp;
-        upTemp->next=NULL;
-        while(temp->next){
-            temp=temp->next;
-        }
-        temp->next=updated->next;
-        return head;
+    ListNode* rotateRight(ListNode* head,int k){
+        if(!head||!head->next||k==0) return head; 
+
+        int len=1;
+        ListNode* tail=head;
+        while(tail->next){ tail=tail->next; len++; } 
+
+        k%=len;
+        if(k==0) return head; 
+
+        tail->next=head;
+
+        int steps=len-k;
+        ListNode* newtail=head;
+        for(int i=1;i<steps;i++) newtail=newtail->next; 
+
+        ListNode* newhead=newtail->next; 
+        newtail->next=NULL;
+
+        return newhead;
     }
 };
 
-//9min, 1wrong: [], 0
+// Comments
