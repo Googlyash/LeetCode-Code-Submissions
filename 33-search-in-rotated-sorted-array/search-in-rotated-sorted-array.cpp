af3447a731ced8422cb701894d88c6d2a944b7e6
Googlyash <77059584+Googlyash@users.noreply.github.com>
2026-05-23 00:01:05 +0530

    Time: 0 ms (100.00%) | Memory: 15.1 MB (66.43%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,29 +1,39 @@
-//Striver: Binary Search
-//POTD Aug'8, 2023 09:39 pm
+// POTD May'22, 2026
+// May'23, 2026 12:00 am
+// Binary Search Practice
 
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int n=nums.size();
-        int l=0, r=n-1, ans=0;
+        int n= nums.size();
+        int l=0, r=n-1;
+
         while(l<=r){
-            int mid=l+(r-l)/2;
-            
-            if(nums[mid]==target)return mid;
-            
-            if(nums[l]<=nums[mid]){
-               if(nums[l]<=target && nums[mid]>=target){
-                  r=mid-1;
-               }
-               else l=mid+1;
+            int mid= l+ (r-l)/2;
+
+            if(nums[mid]==target){
+                return mid;
             }
-            else {
-                if(nums[mid]<=target && target<=nums[r]){
+            if(nums[mid]>=nums[l]){
+                if(nums[mid]>=target && target>=nums[l]){
+                    r= mid-1;
+                }
+                else {
                     l=mid+1;
                 }
-                else r=mid-1;
+            }
+            else {
+                if(nums[r]>=target && target>=nums[mid]){
+                    l=mid+1;
+                }
+                else {
+                    r=mid-1;
+                }
             }
         }
         return -1;
     }
 };
+
+// Earlier Solved
+// 10 min
