950576721daa43f784d3a68095476c939c204802
Googlyash <77059584+Googlyash@users.noreply.github.com>
2026-07-12 02:24:34 +0530

    Time: 78 ms (19.22%) | Memory: 137.9 MB (24.51%) - LeetSync

--- 2793-count-the-number-of-complete-components/count-the-number-of-complete-components.cpp
+++ 2793-count-the-number-of-complete-components/count-the-number-of-complete-components.cpp
@@ -1,37 +1,49 @@
-// POTD Mar'22, 2025
-// Mar'23, 2025 12:18 am
+// POTD Jul'11, 2026
+// Jul'12, 2026 02:25 am
 
 class Solution {
-    void dfs(int x, const vector<vector<int>> &con, vector<bool> &mark, int &a, int &b) {
-        if (mark[x]) {
+    void dfs(int node, int par, vector<int>&vis, set<int>&cur, vector<int>adj[]){
+        if(vis[node]){
             return;
         }
-        mark[x] = true;
-        ++a;
-        b += con[x].size();
-        for (int y : con[x]) {
-            dfs(y, con, mark, a, b);
-        }
-    }
-    
-public:
-    int countCompleteComponents(int n, vector<vector<int>>& edges) {
-        vector<vector<int>> con(n);
-        for (const auto& e : edges) {
-            con[e[0]].push_back(e[1]);
-            con[e[1]].push_back(e[0]);
-        }
-        vector<bool> mark(n);
-        int r = 0;
-        for (int i = 0; i < n; ++i) {
-            if (!mark[i]) {
-                int x = 0, y = 0;
-                dfs(i, con, mark, x, y);
-                r += x * (x - 1) == y;
+        vis[node]=1;
+        cur.insert(node);
+        for(auto neighbor: adj[node]){
+            if(neighbor!= par && !vis[neighbor]){
+                dfs(neighbor, node, vis, cur, adj);
             }
         }
-        return r;
+        return;
+    }
+public:
+    int countCompleteComponents(int n, vector<vector<int>>& edges) {
+        int cnt=0;
+
+        vector<int>adj[n];
+        for(auto it: edges){
+            int u= it[0];
+            int v= it[1];
+
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+
+        vector<int>vis(n, 0);
+        for(int i=0;i<n;i++){
+            if(!vis[i]){
+                set<int>cur;
+                dfs(i, -1, vis, cur, adj);
+                int k= cur.size(), check=0;
+                for(auto it: cur){
+                    if(adj[it].size()==k-1){
+                        check++;
+                    }
+                }
+                if(check==k)cnt++;
+            }
+        }
+        return cnt;
     }
 };
 
-// Comemnts
+// 12 min
