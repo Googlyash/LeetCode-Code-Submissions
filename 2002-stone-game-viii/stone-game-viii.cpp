9de3de0986a8d1caece936c908a2ce13cec92f3e
Googlyash <77059584+Googlyash@users.noreply.github.com>
2026-08-24 21:41:37 +0530

    Time: 105 ms (64.28%) | Memory: 91.8 MB (62.81%) - LeetSync

---
+++ 2002-stone-game-viii/stone-game-viii.cpp
@@ -0,0 +1,25 @@
+// POTD Aug'24, 2026
+// Aug'24, 2026 09:41 pm
+
+class Solution {
+public:
+    int stoneGameVIII(vector<int>& stones) {
+        int n = stones.size();
+
+        vector<int> prefix = stones;
+
+        for (int i = 1; i < n; i++) {
+            prefix[i] += prefix[i - 1];
+        }
+
+        int best = prefix[n - 1];
+
+        for (int i = n - 2; i >= 1; i--) {
+            best = max(best, prefix[i] - best);
+        }
+
+        return best;
+    }
+};
+
+// Comments
