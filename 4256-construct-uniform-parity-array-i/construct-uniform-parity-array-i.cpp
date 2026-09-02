7819e6601203035de4659299672e934cf4c5ea19
Googlyash <77059584+Googlyash@users.noreply.github.com>
2026-09-03 01:19:22 +0530

    Time: 0 ms (100.00%) | Memory: 30.2 MB (67.86%) - LeetSync

---
+++ 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.cpp
@@ -0,0 +1,13 @@
+// POTD Sep'2, 2026
+// Sep'3, 2026 01:19 am
+
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+        int n= nums1.size();
+
+        return true;
+    }
+};
+
+// 3 min
